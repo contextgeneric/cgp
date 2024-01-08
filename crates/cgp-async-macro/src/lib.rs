73ecb8059b6651e8832eaf523f844a2300893b4b
Soares Chen <soares.chen@maybevoid.com>
2024-01-08 10:30:03 +0100

    Implement custom proc macro to transform async fn to return impl Future

--- crates/cgp-async-macro/src/lib.rs
+++ crates/cgp-async-macro/src/lib.rs
@@ -2,14 +2,15 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 
-mod helper;
+mod impl_async;
+mod strip_async;
 
 #[proc_macro_attribute]
 pub fn strip_async(_attr: TokenStream, stream: TokenStream) -> TokenStream {
-    helper::strip_async(stream.into()).into()
+    strip_async::strip_async(stream.into()).into()
 }
 
 #[proc_macro_attribute]
 pub fn native_async(_attr: TokenStream, stream: TokenStream) -> TokenStream {
-    stream
+    impl_async::impl_async(stream.into()).into()
 }
