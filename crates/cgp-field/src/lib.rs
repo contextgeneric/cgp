d7e74ee8e2c129aaa5780fcb75ffeb619682d980
Soares Chen <soares.chen@maybevoid.com>
2024-07-04 10:58:20 +0800

    Reformat imports

--- crates/cgp-field/src/lib.rs
+++ crates/cgp-field/src/lib.rs
@@ -3,7 +3,6 @@
 pub mod traits;
 pub mod types;
 
-pub use cgp_field_macro::derive_fields;
-pub use cgp_field_macro::symbol;
+pub use cgp_field_macro::{derive_fields, symbol};
 pub use traits::HasField;
 pub use types::Char;
