53bebe830dfa60efa3c0d523eaa026bd6345bef4
Soares Chen <soares.chen@maybevoid.com>
2024-09-29 19:26:13 +0000

    Add UseContext

--- crates/cgp-type/src/traits/has_type.rs
+++ crates/cgp-type/src/traits/has_type.rs
@@ -1,8 +1,15 @@
-use cgp_component::{derive_component, DelegateComponent, DelegateTo, HasComponents};
+use cgp_component::{derive_component, DelegateComponent, DelegateTo, HasComponents, UseContext};
 
 #[derive_component(TypeComponent, ProvideType<Context>)]
 pub trait HasType<Tag> {
     type Type;
+}
+
+impl<Context, Tag> ProvideType<Context, Tag> for UseContext
+where
+    Context: HasType<Tag>,
+{
+    type Type = Context::Type;
 }
 
 impl<Context, Tag, Components, Type> ProvideType<Context, Tag> for DelegateTo<Components>
