ed70a58e6e1241a0f382400ecab212115a7db120
Soares Chen <soares.chen@maybevoid.com>
2024-09-29 18:06:03 +0000

    Add HasFieldMut trait

--- crates/cgp-field/src/traits/has_field.rs
+++ crates/cgp-field/src/traits/has_field.rs
@@ -1,10 +1,14 @@
 use core::marker::PhantomData;
-use core::ops::Deref;
+use core::ops::{Deref, DerefMut};
 
 pub trait HasField<Tag> {
     type Field;
 
     fn get_field(&self, tag: PhantomData<Tag>) -> &Self::Field;
+}
+
+pub trait HasFieldMut<Tag>: HasField<Tag> {
+    fn get_field_mut(&mut self, tag: PhantomData<Tag>) -> &mut Self::Field;
 }
 
 impl<Context, Tag, Target, Field> HasField<Tag> for Context
@@ -16,5 +20,15 @@ where
 
     fn get_field(&self, tag: PhantomData<Tag>) -> &Self::Field {
         self.deref().get_field(tag)
+    }
+}
+
+impl<Context, Tag, Target, Field> HasFieldMut<Tag> for Context
+where
+    Context: DerefMut<Target = Target>,
+    Target: HasFieldMut<Tag, Field = Field> + 'static,
+{
+    fn get_field_mut(&mut self, tag: PhantomData<Tag>) -> &mut Self::Field {
+        self.deref_mut().get_field_mut(tag)
     }
 }
