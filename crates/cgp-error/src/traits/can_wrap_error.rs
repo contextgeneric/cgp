e5d70e281a089a33a6b94a923a4700fb101fd2e2
Soares Chen <soares.chen@maybevoid.com>
2025-01-05 20:47:39 +0000

    Add CanWrapError trait

---
+++ crates/cgp-error/src/traits/can_wrap_error.rs
@@ -0,0 +1,22 @@
+use cgp_component::{DelegateComponent, HasComponents, UseDelegate};
+use cgp_component_macro::cgp_component;
+
+use crate::HasErrorType;
+
+#[cgp_component {
+    provider: ErrorWrapper,
+}]
+pub trait CanWrapError<Detail>: HasErrorType {
+    fn wrap_error(error: Self::Error, detail: Detail) -> Self::Error;
+}
+
+impl<Context, Detail, Components> ErrorWrapper<Context, Detail> for UseDelegate<Components>
+where
+    Context: HasErrorType,
+    Components: DelegateComponent<Detail>,
+    Components::Delegate: ErrorWrapper<Context, Detail>,
+{
+    fn wrap_error(error: Context::Error, detail: Detail) -> Context::Error {
+        Components::Delegate::wrap_error(error, detail)
+    }
+}
--- crates/cgp-error/src/traits/mod.rs
+++ crates/cgp-error/src/traits/mod.rs
@@ -1,7 +1,9 @@
 pub mod can_raise_error;
+pub mod can_wrap_error;
 pub mod has_async_error_type;
 pub mod has_error_type;
 
 pub use can_raise_error::*;
+pub use can_wrap_error::*;
 pub use has_async_error_type::*;
 pub use has_error_type::*;
