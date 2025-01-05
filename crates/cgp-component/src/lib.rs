b2a1b00f697ed343e5309ddebe2c5afc16668ced
Soares Chen <soares.chen@maybevoid.com>
2025-01-05 21:47:42 +0000

    Fix documentation

--- crates/cgp-component/src/lib.rs
+++ crates/cgp-component/src/lib.rs
@@ -2,7 +2,6 @@
 
 /*!
    This crate defines the core CGP traits, [`DelegateComponent`] and [`HasComponents`].
-   It also re-export component macros provided by [`cgp_component_macro`].
 */
 
 pub mod traits;
