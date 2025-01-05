ec459ce92d312bd351766aa726236aab01444b4b
Soares Chen <soares.chen@maybevoid.com>
2025-01-05 21:51:06 +0000

    Implement Display for Index

--- crates/cgp-field/src/types/index.rs
+++ crates/cgp-field/src/types/index.rs
@@ -1,1 +1,9 @@
+use core::fmt::Display;
+
 pub struct Index<const I: usize>;
+
+impl<const I: usize> Display for Index<I> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "{I}")
+    }
+}
