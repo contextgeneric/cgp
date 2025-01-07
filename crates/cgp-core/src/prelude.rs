a444258387f4989f50c3c7f6b6682bc53d1343f4
Soares Chen <soares.chen@maybevoid.com>
2025-01-07 16:02:40 +0000

    Use cgp_type! to derive HasRuntimeType

--- crates/cgp-core/src/prelude.rs
+++ crates/cgp-core/src/prelude.rs
@@ -1,9 +1,9 @@
 pub use cgp_async::{async_trait, Async, MaybeSend, MaybeStatic, MaybeSync};
 pub use cgp_component::{DelegateComponent, HasComponents, WithContext, WithProvider};
 pub use cgp_component_macro::{
-    cgp_component, cgp_preset, delegate_components, for_each_replace, replace_with,
+    cgp_component, cgp_preset, cgp_type, delegate_components, for_each_replace, replace_with,
 };
 pub use cgp_error::{CanRaiseError, CanWrapError, HasErrorType};
 pub use cgp_field::{Char, Cons, Either, HasField, HasFieldMut, Nil, Void};
 pub use cgp_field_macro::{product, symbol, HasField, Product, Sum};
-pub use cgp_type::{HasType, UseType};
+pub use cgp_type::{HasType, ProvideType, UseType};
--- crates/cgp-runtime/src/traits/has_runtime_type.rs
+++ crates/cgp-runtime/src/traits/has_runtime_type.rs
@@ -1,20 +1,5 @@
-use cgp_core::component::WithProvider;
 use cgp_core::prelude::*;
-use cgp_core::types::ProvideType;
 
-#[cgp_component {
-    name: RuntimeTypeComponent,
-    provider: ProvideRuntimeType,
-}]
-pub trait HasRuntimeType {
-    type Runtime;
-}
-
-pub type RuntimeOf<Context> = <Context as HasRuntimeType>::Runtime;
-
-impl<Context, Provider, Runtime> ProvideRuntimeType<Context> for WithProvider<Provider>
-where
-    Provider: ProvideType<Context, RuntimeTypeComponent, Type = Runtime>,
-{
-    type Runtime = Runtime;
+cgp_type! {
+    Runtime
 }
