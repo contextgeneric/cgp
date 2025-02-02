261f3c64a0d65cdc9faad1d6edee7ff7136e003a
Soares Chen <soares.chen@maybevoid.com>
2025-02-02 18:59:57 +0000

    Use __Context__ instead of Context inside derive_type

--- crates/cgp-component-macro-lib/src/type_component/derive.rs
+++ crates/cgp-component-macro-lib/src/type_component/derive.rs
@@ -69,7 +69,7 @@ pub fn do_derive_type_component(
     let component_name = Ident::new(&format!("{ident}TypeComponent"), ident.span());
 
     let alias_type: ItemType = parse_quote! {
-        pub type #alias_name <Context> = <Context as #consumer_trait_name>:: #ident;
+        pub type #alias_name <__Context__> = <__Context__ as #consumer_trait_name>:: #ident;
     };
 
     let mut consumer_trait: ItemTrait = parse_quote! {
@@ -81,28 +81,28 @@ pub fn do_derive_type_component(
     consumer_trait.attrs = attributes;
 
     let provider_trait: ItemTrait = parse_quote! {
-        pub trait #provider_trait_name <Context> {
+        pub trait #provider_trait_name <__Context__> {
             type #ident : #bounds;
         }
     };
 
     let consumer_impl: ItemImpl = parse_quote! {
-        impl<Context, Components> #consumer_trait_name for Context
+        impl<__Context__, __Components__> #consumer_trait_name for __Context__
         where
-            Context: HasComponents< Components = Components >,
-            Components: #provider_trait_name <Context>,
-            Components:: #ident : #bounds,
+            __Context__: HasComponents< Components = __Components__ >,
+            __Components__: #provider_trait_name <__Context__>,
+            __Components__:: #ident : #bounds,
         {
-            type #ident = Components:: #ident;
+            type #ident = __Components__:: #ident;
         }
     };
 
     let provider_impl: ItemImpl = parse_quote! {
-        impl<Context, Component, Delegate>
-            #provider_trait_name <Context> for Component
+        impl<__Context__, Component, Delegate>
+            #provider_trait_name <__Context__> for Component
         where
             Component: DelegateComponent< #component_name, Delegate = Delegate >,
-            Delegate: #provider_trait_name <Context>,
+            Delegate: #provider_trait_name <__Context__>,
             Delegate:: #ident : #bounds,
         {
             type #ident = Delegate:: #ident;
@@ -110,10 +110,10 @@ pub fn do_derive_type_component(
     };
 
     let with_provider_impl: ItemImpl = parse_quote! {
-        impl<Context, Provider, #ident> #provider_trait_name <Context>
+        impl<__Context__, Provider, #ident> #provider_trait_name <__Context__>
             for WithProvider<Provider>
         where
-            Provider: ProvideType<Context, #component_name, Type = #ident >,
+            Provider: ProvideType<__Context__, #component_name, Type = #ident >,
             #ident: #bounds,
         {
             type #ident = #ident;
@@ -124,7 +124,7 @@ pub fn do_derive_type_component(
         derive_is_provider_for(&parse_quote!(#component_name), &with_provider_impl)?;
 
     let use_type_impl: ItemImpl = parse_quote! {
-        impl<Context, #ident> #provider_trait_name <Context>
+        impl<__Context__, #ident> #provider_trait_name <__Context__>
             for UseType<#ident>
         where
             #ident: #bounds,
