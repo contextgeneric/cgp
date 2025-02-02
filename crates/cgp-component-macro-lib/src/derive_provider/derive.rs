8ce41c4947f94ecc3e082146b4c46849689b7c96
Soares Chen <soares.chen@maybevoid.com>
2025-02-02 16:04:52 +0000

    Initial implementation for derive_provider

--- crates/cgp-component-macro-lib/src/derive_provider/derive.rs
+++ crates/cgp-component-macro-lib/src/derive_provider/derive.rs
@@ -1,5 +1,75 @@
-use proc_macro2::TokenStream;
+use proc_macro2::{Span, TokenStream};
+use quote::quote;
+use syn::spanned::Spanned;
+use syn::token::For;
+use syn::{parse_quote, Error, GenericArgument, ItemImpl, Path, PathArguments, Type};
 
-pub fn derive_provider(attr: TokenStream, item: TokenStream) -> syn::Result<TokenStream> {
-    todo!()
+pub fn derive_provider(attr: TokenStream, body: TokenStream) -> syn::Result<TokenStream> {
+    let component_name: Type = syn::parse2(attr)?;
+
+    let provider_impl: ItemImpl = syn::parse2(body)?;
+
+    let is_provider_for_impl: ItemImpl = derive_is_provider_for(&component_name, &provider_impl)?;
+
+    let result = quote! {
+        #provider_impl
+        #is_provider_for_impl
+    };
+
+    Ok(result)
+}
+
+pub fn derive_is_provider_for(
+    component_name: &Type,
+    provider_impl: &ItemImpl,
+) -> syn::Result<ItemImpl> {
+    let provider_path = provider_impl
+        .trait_
+        .as_ref()
+        .ok_or_else(|| {
+            Error::new(
+                provider_impl.span(),
+                "provider impl should contain trait path",
+            )
+        })?
+        .1
+        .segments
+        .last()
+        .ok_or_else(|| {
+            Error::new(
+                provider_impl.span(),
+                "provider impl should contain trait path containing generic parameters",
+            )
+        })?;
+
+    let is_provider_generics = match &provider_path.arguments {
+        PathArguments::AngleBracketed(generics) => {
+            let mut generics = generics.clone();
+
+            generics
+                .args
+                .insert(0, GenericArgument::Type(component_name.clone()));
+
+            generics
+        }
+        _ => {
+            return Err(Error::new(
+                provider_impl.span(),
+                "provider impl should contain trait path containing generic parameters",
+            ));
+        }
+    };
+
+    let is_provider_path: Path = parse_quote!( IsProviderFor #is_provider_generics );
+
+    let mut is_provider_impl = provider_impl.clone();
+
+    is_provider_impl.attrs.clear();
+    is_provider_impl.items.clear();
+    is_provider_impl.defaultness = None;
+    is_provider_impl.unsafety = None;
+
+    is_provider_impl.trait_ = Some((None, is_provider_path, For(Span::call_site())));
+
+    Ok(is_provider_impl)
 }
