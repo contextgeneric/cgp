b53589d7d2b4dc8bcdc74a2e033675e85281af99
Soares Chen <soares.chen@maybevoid.com>
2025-02-03 20:32:28 +0000

    Replace provider constraints inside generic params

--- crates/cgp-component-macro-lib/src/derive_provider/replace_constraint.rs
+++ crates/cgp-component-macro-lib/src/derive_provider/replace_constraint.rs
@@ -1,13 +1,19 @@
 use std::collections::BTreeMap;
 
 use syn::punctuated::Punctuated;
-use syn::token::Comma;
+use syn::token::{Comma, Plus};
 use syn::{
-    parse_quote, GenericArgument, Generics, Ident, PathArguments, Type, TypeParamBound,
-    WherePredicate,
+    parse_quote, GenericArgument, GenericParam, Generics, Ident, PathArguments, Type,
+    TypeParamBound, WherePredicate,
 };
 
 pub fn replace_provider_in_generics(provider_map: &BTreeMap<Ident, Type>, generics: &mut Generics) {
+    for param in &mut generics.params {
+        if let GenericParam::Type(type_param) = param {
+            replace_provider_in_type_params(provider_map, &mut type_param.bounds);
+        }
+    }
+
     if let Some(where_clause) = &mut generics.where_clause {
         replace_provider_in_where_predicate(provider_map, &mut where_clause.predicates);
     }
@@ -19,19 +25,26 @@ pub fn replace_provider_in_where_predicate(
 ) {
     for predicate in predicates.iter_mut() {
         if let WherePredicate::Type(type_predicate) = predicate {
-            for bound in type_predicate.bounds.iter_mut() {
-                if let TypeParamBound::Trait(trait_bound) = bound {
-                    if let Some(segment) = trait_bound.path.segments.last_mut() {
-                        if let Some(component_type) = provider_map.get(&segment.ident).cloned() {
-                            if let PathArguments::AngleBracketed(args) = &segment.arguments {
-                                let mut generics = args.args.iter().map(Clone::clone);
-                                if let Some(GenericArgument::Type(context_type)) = generics.next() {
-                                    let rest_generics: Punctuated<GenericArgument, Comma> =
-                                        generics.collect();
+            replace_provider_in_type_params(provider_map, &mut type_predicate.bounds);
+        }
+    }
+}
 
-                                    trait_bound.path = parse_quote!( IsProviderFor< #component_type, #context_type, (#rest_generics) > );
-                                }
-                            }
+pub fn replace_provider_in_type_params(
+    provider_map: &BTreeMap<Ident, Type>,
+    type_params: &mut Punctuated<TypeParamBound, Plus>,
+) {
+    for bound in type_params.iter_mut() {
+        if let TypeParamBound::Trait(trait_bound) = bound {
+            if let Some(segment) = trait_bound.path.segments.last_mut() {
+                if let Some(component_type) = provider_map.get(&segment.ident).cloned() {
+                    if let PathArguments::AngleBracketed(args) = &segment.arguments {
+                        let mut generics = args.args.iter().map(Clone::clone);
+                        if let Some(GenericArgument::Type(context_type)) = generics.next() {
+                            let rest_generics: Punctuated<GenericArgument, Comma> =
+                                generics.collect();
+
+                            trait_bound.path = parse_quote!( IsProviderFor< #component_type, #context_type, (#rest_generics) > );
                         }
                     }
                 }
