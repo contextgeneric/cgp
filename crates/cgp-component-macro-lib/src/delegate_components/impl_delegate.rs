460024d9fffd555110ef4ba4929c66428482a871
Soares Chen <soares.chen@maybevoid.com>
2025-02-03 21:16:43 +0000

    Check flags in impl for provider

--- crates/cgp-component-macro-lib/src/delegate_components/impl_delegate.rs
+++ crates/cgp-component-macro-lib/src/delegate_components/impl_delegate.rs
@@ -6,7 +6,6 @@ use syn::{parse_quote, Generics, ImplItem, ImplItemType, ItemImpl, Path, Type};
 
 use crate::delegate_components::ast::{ComponentAst, DelegateEntriesAst};
 use crate::delegate_components::merge_generics::merge_generics;
-use crate::derive_provider::ENABLE_IS_PROVIDER_SUPERTRAIT;
 
 pub fn impl_delegate_components(
     target_type: &Type,
@@ -46,12 +45,10 @@ pub fn impl_delegate_component(
         generics.params.push(parse_quote!(__Context__));
         generics.params.push(parse_quote!(__Params__));
 
-        if ENABLE_IS_PROVIDER_SUPERTRAIT {
-            let where_clause = generics.make_where_clause();
-            where_clause.predicates.push(
-                parse_quote!( #source : IsProviderFor< #component_type, __Context__, __Params__ > ),
-            );
-        }
+        let where_clause = generics.make_where_clause();
+        where_clause.predicates.push(
+            parse_quote!( #source : IsProviderFor< #component_type, __Context__, __Params__ > ),
+        );
 
         generics
     };
--- crates/cgp-component-macro-lib/src/derive_component/provider_impl.rs
+++ crates/cgp-component-macro-lib/src/derive_component/provider_impl.rs
@@ -12,6 +12,7 @@ use syn::{
 use crate::derive_component::delegate_fn::derive_delegated_fn_impl;
 use crate::derive_component::delegate_type::derive_delegate_type_impl;
 use crate::derive_component::generic_args::extract_generic_args;
+use crate::derive_provider::ENABLE_IS_PROVIDER_SUPERTRAIT;
 
 pub fn derive_provider_impl(
     context_type: &Ident,
@@ -36,10 +37,15 @@ pub fn derive_provider_impl(
         {
             let is_provider_params = extract_generic_args(&consumer_trait.generics.params);
 
-            let delegate_constraint: Punctuated<TypeParamBound, Plus> = parse_quote! {
+            let mut delegate_constraint: Punctuated<TypeParamBound, Plus> = parse_quote! {
                 DelegateComponent< #component_name < #component_params > >
-                + IsProviderFor< #component_name < #component_params >, #context_type, ( #is_provider_params ) >
             };
+
+            if ENABLE_IS_PROVIDER_SUPERTRAIT {
+                delegate_constraint.push(parse_quote!(
+                    IsProviderFor< #component_name < #component_params >, #context_type, ( #is_provider_params ) >
+                ))
+            }
 
             let provider_constraint: Punctuated<TypeParamBound, Plus> = parse_quote! {
                 #provider_name < #provider_generic_args >
