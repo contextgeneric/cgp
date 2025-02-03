62c5dc7eccea3f36b7d1718a03c64c668ef08527
Soares Chen <soares.chen@maybevoid.com>
2025-02-03 21:28:48 +0000

    Add diagnostic::on_unimplemented for DelegateComponent

--- crates/cgp-component/src/traits/delegate_component.rs
+++ crates/cgp-component/src/traits/delegate_component.rs
@@ -1,3 +1,7 @@
+#[diagnostic::on_unimplemented(
+    message = "{Self} does not contain any DelegateComponent entry for {Name}",
+    note = "You might want to implement the provider trait for {Name} on {Self}"
+)]
 pub trait DelegateComponent<Name> {
     type Delegate;
 }
