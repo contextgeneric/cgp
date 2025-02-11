57eea2e75bcfee50848d2707c593caf526231c34
Soares Chen <soares.chen@maybevoid.com>
2025-02-11 19:08:34 +0000

    Set default type of IsProviderFor<Params = ()>

--- crates/cgp-component/src/traits/is_provider.rs
+++ crates/cgp-component/src/traits/is_provider.rs
@@ -1,4 +1,4 @@
 #[diagnostic::on_unimplemented(
     note = "You need to add `#[cgp_provider({Component})]` on the impl block for CGP provider traits"
 )]
-pub trait IsProviderFor<Component, Context, Params> {}
+pub trait IsProviderFor<Component, Context, Params = ()> {}
