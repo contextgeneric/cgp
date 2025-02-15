a241a0effac8ea2015b3cc5b34836479d7b09dfa
Soares Chen <soares.chen@maybevoid.com>
2025-02-15 19:07:40 +0000

    Put generated items inside child module

--- crates/cgp-component-macro-lib/src/preset/define_preset.rs
+++ crates/cgp-component-macro-lib/src/preset/define_preset.rs
@@ -1,14 +1,10 @@
-use alloc::format;
-use alloc::string::ToString;
-
 use proc_macro2::{Span, TokenStream};
 use quote::{quote, ToTokens, TokenStreamExt};
-use syn::{parse_quote, Generics, Ident, ItemTrait};
+use syn::{parse2, parse_quote, Generics, Ident, ItemTrait};
 
 use crate::delegate_components::define_struct::define_struct;
 use crate::delegate_components::delegates_to::define_delegates_to_trait;
 use crate::delegate_components::impl_delegate::impl_delegate_components;
-use crate::derive_component::snake_case::to_snake_case_str;
 use crate::preset::ast::DefinePresetAst;
 use crate::preset::impl_is_preset::impl_components_is_preset;
 use crate::preset::substitution_macro::define_substitution_macro;
@@ -16,24 +12,35 @@ use crate::preset::substitution_macro::define_substitution_macro;
 pub fn define_preset(body: TokenStream) -> syn::Result<TokenStream> {
     let ast: DefinePresetAst = syn::parse2(body)?;
 
-    let preset_ident = &ast.preset.name;
+    let preset_module_name = &ast.preset.name;
+
     let preset_generic_args = &ast.preset.generics;
 
     let preset_generics: Generics = syn::parse2(quote!( #preset_generic_args ))?;
 
+    let preset_struct_name = Ident::new("Preset", Span::call_site());
+
     let preset_type = {
         let type_generics = preset_generics.split_for_impl().1;
-        parse_quote!( #preset_ident #type_generics )
+        parse2(quote! { #preset_struct_name #type_generics })?
     };
 
-    let preset_trait_name = Ident::new(&format!("Is{}", preset_ident), preset_ident.span());
+    let preset_trait_name = Ident::new("IsPreset", Span::call_site());
 
     let preset_trait: ItemTrait = parse_quote! {
         pub trait #preset_trait_name <Component> {}
     };
 
     let impl_delegate_items = {
-        let items = impl_delegate_components(&preset_type, &preset_generics, &ast.delegate_entries);
+        let namespaces_preset_type = parse2(quote! {
+            #preset_module_name :: #preset_type
+        })?;
+
+        let items = impl_delegate_components(
+            &namespaces_preset_type,
+            &preset_generics,
+            &ast.delegate_entries,
+        );
 
         let mut stream = TokenStream::new();
         stream.append_all(items);
@@ -48,19 +55,21 @@ pub fn define_preset(body: TokenStream) -> syn::Result<TokenStream> {
         &ast.delegate_entries,
     );
 
-    let item_struct = define_struct(preset_ident, &preset_generics);
+    let preset_struct = define_struct(&preset_struct_name, &preset_generics);
 
     let mut mod_output = quote! {
+        #preset_struct
+
         #preset_trait
     };
 
     mod_output.append_all(impl_is_preset_items);
 
     {
-        let delegates_to_trait_name = format!("DelegatesTo{}", preset_ident);
+        let delegates_to_trait_name = Ident::new("DelegatesToPreset", Span::call_site());
 
         let (delegates_to_trait, delegates_to_impl) = define_delegates_to_trait(
-            &Ident::new(&delegates_to_trait_name, Span::call_site()),
+            &delegates_to_trait_name,
             &preset_type,
             &preset_generics,
             &ast.delegate_entries,
@@ -71,11 +80,10 @@ pub fn define_preset(body: TokenStream) -> syn::Result<TokenStream> {
     }
 
     {
-        let with_components_macro_name =
-            format!("with_{}", to_snake_case_str(&preset_ident.to_string()));
+        let with_components_macro_name = Ident::new("with_components", Span::call_site());
 
         let with_components_macro = define_substitution_macro(
-            &Ident::new(&with_components_macro_name, Span::call_site()),
+            &with_components_macro_name,
             &ast.delegate_entries.all_components().to_token_stream(),
         );
 
@@ -83,17 +91,13 @@ pub fn define_preset(body: TokenStream) -> syn::Result<TokenStream> {
     }
 
     let output = quote! {
-        #item_struct
-
-        #impl_delegate_items
-
-        mod preset {
+        pub mod #preset_module_name {
             use super::*;
 
             #mod_output
         }
 
-        pub use preset::*;
+        #impl_delegate_items
     };
 
     Ok(output)
