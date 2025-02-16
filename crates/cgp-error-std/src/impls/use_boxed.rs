497097c85e0ed1233d7c003019213d09986eaad3
Soares Chen <soares.chen@maybevoid.com>
2025-02-16 18:16:25 +0000

    Fix missing cgp_provider attrs

--- crates/cgp-error-std/src/impls/use_boxed.rs
+++ crates/cgp-error-std/src/impls/use_boxed.rs
@@ -1,9 +1,11 @@
-use cgp_core::error::ErrorTypeProvider;
+use cgp_core::error::{ErrorTypeProvider, ErrorTypeProviderComponent};
+use cgp_core::prelude::*;
 
 use crate::types::Error;
 
 pub struct UseBoxedStdError;
 
+#[cgp_provider(ErrorTypeProviderComponent)]
 impl<Context> ErrorTypeProvider<Context> for UseBoxedStdError {
     type Error = Error;
 }
