33b15bd4de86f68b2b44d4be8b07723fac1bf8ef
Soares Chen <soares.chen@maybevoid.com>
2025-02-22 15:28:10 +0000

    Re-export imports inside a re_export submodule

--- crates/cgp-component-macro-lib/src/re_export_imports.rs
+++ crates/cgp-component-macro-lib/src/re_export_imports.rs
@@ -1,36 +1,48 @@
 use proc_macro2::{Span, TokenStream};
-use quote::quote;
+use quote::{quote, TokenStreamExt};
 use syn::token::Pub;
-use syn::{parse2, parse_quote, Error, Item, ItemMod, Visibility};
+use syn::{parse2, parse_quote, Attribute, Ident, Item, ItemMod, ItemUse, Visibility};
 
 pub fn derive_re_export_imports(attrs: TokenStream, body: TokenStream) -> syn::Result<TokenStream> {
-    if !attrs.is_empty() {
-        return Err(Error::new_spanned(
-            attrs,
-            "#[re_export_imports] do not accept any attribute argument",
-        ));
-    }
+    let export_mod_name: Ident = if !attrs.is_empty() {
+        parse2(attrs)?
+    } else {
+        Ident::new("re_exports", Span::call_site())
+    };
 
-    let mut item_mod: ItemMod = parse2(body)?;
+    let mut re_exports: Vec<ItemUse> = Vec::new();
+
+    let item_mod: ItemMod = parse2(body)?;
 
     let mod_name = &item_mod.ident;
 
-    if let Some(content) = &mut item_mod.content {
-        for item in content.1.iter_mut() {
+    let doc_hidden: Attribute = parse_quote! { #[doc(hidden)] };
+
+    if let Some(content) = &item_mod.content {
+        for item in content.1.iter() {
             if let Item::Use(use_item) = item {
-                match &use_item.vis {
-                    Visibility::Public(_) => {}
-                    _ => {
-                        use_item.vis = Visibility::Public(Pub(Span::call_site()));
-                        use_item.attrs.push(parse_quote!( #[doc(hidden)] ));
-                    }
-                }
+                let mut re_export = use_item.clone();
+                re_export.vis = Visibility::Public(Pub(Span::call_site()));
+                re_export.attrs.push(doc_hidden.clone());
+                re_exports.push(re_export);
             }
         }
     }
 
+    let mut mod_body = TokenStream::new();
+    mod_body.append_all(re_exports);
+
+    let export_mod: ItemMod = parse2(quote! {
+        #[doc(hidden)]
+        pub mod #export_mod_name {
+            #mod_body
+        }
+    })?;
+
     let out = quote! {
         #item_mod
+
+        #export_mod
 
         pub use #mod_name ::*;
     };
