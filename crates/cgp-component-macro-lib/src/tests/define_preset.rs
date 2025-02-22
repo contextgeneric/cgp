ac75e6d26e684bd36e8eeb11119140ae31d9df3f
Soares Chen <soares.chen@maybevoid.com>
2025-02-22 20:38:42 +0000

    Fix test

--- crates/cgp-component-macro-lib/src/tests/define_preset.rs
+++ crates/cgp-component-macro-lib/src/tests/define_preset.rs
@@ -101,178 +101,155 @@ fn test_define_preset_containing_generics() {
     .unwrap();
 
     let expected = quote! {
-        pub struct FooPreset<'a, FooParamA, FooParamB>(
-            pub ::core::marker::PhantomData<(&'a (), FooParamA, FooParamB)>,
-        );
-
-        pub trait IsFooPreset<Component> {}
-
         impl<'a, FooParamA, FooParamB: FooConstraint> DelegateComponent<BarComponentA>
-            for FooPreset<'a, FooParamA, FooParamB>
+            for FooPreset::Provider<'a, FooParamA, FooParamB>
         {
             type Delegate = BazComponentsA<FooParamA>;
         }
 
         impl<'a, FooParamA, FooParamB: FooConstraint, __Context__, __Params__>
             IsProviderFor<BarComponentA, __Context__, __Params__>
-            for FooPreset<'a, FooParamA, FooParamB>
+            for FooPreset::Provider<'a, FooParamA, FooParamB>
         where
             BazComponentsA<FooParamA>: IsProviderFor<BarComponentA, __Context__, __Params__>,
-        {}
+        {
+        }
 
         impl<'a, FooParamA, FooParamB: FooConstraint> DelegateComponent<BarComponentB<'a>>
-            for FooPreset<'a, FooParamA, FooParamB>
+            for FooPreset::Provider<'a, FooParamA, FooParamB>
         {
             type Delegate = BazComponentsB;
         }
 
         impl<'a, FooParamA, FooParamB: FooConstraint, __Context__, __Params__>
             IsProviderFor<BarComponentB<'a>, __Context__, __Params__>
-            for FooPreset<'a, FooParamA, FooParamB>
+            for FooPreset::Provider<'a, FooParamA, FooParamB>
         where
             BazComponentsB: IsProviderFor<BarComponentB<'a>, __Context__, __Params__>,
-        {}
+        {
+        }
 
         impl<'a, FooParamA, FooParamB: FooConstraint> DelegateComponent<BarComponentC<FooParamB>>
-            for FooPreset<'a, FooParamA, FooParamB>
+            for FooPreset::Provider<'a, FooParamA, FooParamB>
         {
             type Delegate = BazComponentsB;
         }
 
         impl<'a, FooParamA, FooParamB: FooConstraint, __Context__, __Params__>
             IsProviderFor<BarComponentC<FooParamB>, __Context__, __Params__>
-            for FooPreset<'a, FooParamA, FooParamB>
+            for FooPreset::Provider<'a, FooParamA, FooParamB>
         where
             BazComponentsB: IsProviderFor<BarComponentC<FooParamB>, __Context__, __Params__>,
-        {}
+        {
+        }
 
-        impl<
-            'a,
-            FooParamA,
-            FooParamB: FooConstraint,
-            BarParamA,
-        > DelegateComponent<BarComponentD<BarParamA, FooParamA>>
-            for FooPreset<'a, FooParamA, FooParamB>
+        impl<'a, FooParamA, FooParamB: FooConstraint, BarParamA>
+            DelegateComponent<BarComponentD<BarParamA, FooParamA>>
+            for FooPreset::Provider<'a, FooParamA, FooParamB>
         {
             type Delegate = BazComponentsB;
         }
 
-        impl<
-            'a,
-            FooParamA,
-            FooParamB: FooConstraint,
-            BarParamA,
-            __Context__,
-            __Params__,
-        >
+        impl<'a, FooParamA, FooParamB: FooConstraint, BarParamA, __Context__, __Params__>
             IsProviderFor<BarComponentD<BarParamA, FooParamA>, __Context__, __Params__>
-            for FooPreset<'a, FooParamA, FooParamB>
+            for FooPreset::Provider<'a, FooParamA, FooParamB>
         where
             BazComponentsB: IsProviderFor<BarComponentD<BarParamA, FooParamA>, __Context__, __Params__>,
-        {}
+        {
+        }
 
-        impl<
-            'a,
-            'b,
-            FooParamA,
-            FooParamB: FooConstraint,
-            BarParamB: BarConstraint,
-        > DelegateComponent<BarComponentE<'b, BarParamB, FooParamB>>
-            for FooPreset<'a, FooParamA, FooParamB>
+        impl<'a, 'b, FooParamA, FooParamB: FooConstraint, BarParamB: BarConstraint>
+            DelegateComponent<BarComponentE<'b, BarParamB, FooParamB>>
+            for FooPreset::Provider<'a, FooParamA, FooParamB>
         {
             type Delegate = BazComponentsB;
         }
 
         impl<
-            'a,
-            'b,
-            FooParamA,
-            FooParamB: FooConstraint,
-            BarParamB: BarConstraint,
-            __Context__,
-            __Params__,
-        >
-            IsProviderFor<BarComponentE<'b, BarParamB, FooParamB>, __Context__, __Params__>
-            for FooPreset<'a, FooParamA, FooParamB>
+                'a,
+                'b,
+                FooParamA,
+                FooParamB: FooConstraint,
+                BarParamB: BarConstraint,
+                __Context__,
+                __Params__,
+            > IsProviderFor<BarComponentE<'b, BarParamB, FooParamB>, __Context__, __Params__>
+            for FooPreset::Provider<'a, FooParamA, FooParamB>
         where
-            BazComponentsB: IsProviderFor<BarComponentE<'b, BarParamB, FooParamB>, __Context__, __Params__>,
-        {}
-
-        impl<T> IsFooPreset<BarComponentA> for T {}
-        impl<T> IsFooPreset<BarComponentB<'a>> for T {}
-        impl<T> IsFooPreset<BarComponentC<FooParamB>> for T {}
-
-        impl<BarParamA, T> IsFooPreset<BarComponentD<BarParamA, FooParamA>> for T {}
-
-        impl<'b, BarParamB: BarConstraint, T> IsFooPreset<BarComponentE<'b, BarParamB, FooParamB>> for T {}
-
-        pub trait DelegatesToFooPreset<
-            'a,
-            FooParamA,
-            FooParamB: FooConstraint,
-        >: DelegateComponent<
-                BarComponentA,
-                Delegate = FooPreset<'a, FooParamA, FooParamB>,
-            > + DelegateComponent<
-                BarComponentB<'a>,
-                Delegate = FooPreset<'a, FooParamA, FooParamB>,
-            > + DelegateComponent<
-                BarComponentC<FooParamB>,
-                Delegate = FooPreset<'a, FooParamA, FooParamB>,
-            > + DelegateComponent<
-                BarComponentD<BarParamA, FooParamA>,
-                Delegate = FooPreset<'a, FooParamA, FooParamB>,
-            > + DelegateComponent<
-                BarComponentE<'b, BarParamB, FooParamB>,
-                Delegate = FooPreset<'a, FooParamA, FooParamB>,
-            > {}
-
-        impl<
-            'a,
-            FooParamA,
-            FooParamB: FooConstraint,
-            Components,
-        > DelegatesToFooPreset<'a, FooParamA, FooParamB> for Components
-        where
-            Components: DelegateComponent<
-                    BarComponentA,
-                    Delegate = FooPreset<'a, FooParamA, FooParamB>,
-                >
-                + DelegateComponent<
-                    BarComponentB<'a>,
-                    Delegate = FooPreset<'a, FooParamA, FooParamB>,
-                >
-                + DelegateComponent<
-                    BarComponentC<FooParamB>,
-                    Delegate = FooPreset<'a, FooParamA, FooParamB>,
-                >
-                + DelegateComponent<
-                    BarComponentD<BarParamA, FooParamA>,
-                    Delegate = FooPreset<'a, FooParamA, FooParamB>,
-                >
-                + DelegateComponent<
-                    BarComponentE<'b, BarParamB, FooParamB>,
-                    Delegate = FooPreset<'a, FooParamA, FooParamB>,
-                >,
-        {}
-
-        #[macro_export]
-        macro_rules! with_foo_preset {
-            ($($body:tt)*) => {
-                replace_with! {
-                    [
-                        BarComponentA,
-                        BarComponentB<'a>,
-                        BarComponentC<FooParamB>,
-                        <BarParamA> BarComponentD<BarParamA, FooParamA>,
-                        <'b, BarParamB: BarConstraint> BarComponentE<'b, BarParamB, FooParamB>
-                    ],
-                    $( $body )*
-                }
-            };
+            BazComponentsB:
+                IsProviderFor<BarComponentE<'b, BarParamB, FooParamB>, __Context__, __Params__>,
+        {
         }
 
-        pub use with_foo_preset;
+        #[allow(non_snake_case)]
+        pub mod FooPreset {
+            use super::*;
+
+            #[doc(hidden)]
+            pub mod re_exports {
+                #[doc(hidden)]
+                #[doc(no_inline)]
+                pub use super::super::super::re_exports::*;
+            }
+
+            pub struct Provider<'a, FooParamA, FooParamB>(
+                pub ::core::marker::PhantomData<(&'a (), FooParamA, FooParamB)>,
+            );
+
+            pub trait IsPreset<Component> {}
+
+            impl<T> IsPreset<BarComponentA> for T {}
+            impl<T> IsPreset<BarComponentB<'a>> for T {}
+            impl<T> IsPreset<BarComponentC<FooParamB>> for T {}
+            impl<BarParamA, T> IsPreset<BarComponentD<BarParamA, FooParamA>> for T {}
+            impl<'b, BarParamB: BarConstraint, T> IsPreset<BarComponentE<'b, BarParamB, FooParamB>> for T {}
+
+            pub trait DelegatesToPreset<'a, FooParamA, FooParamB: FooConstraint>:
+                DelegateComponent<BarComponentA, Delegate = Provider<'a, FooParamA, FooParamB>>
+                + DelegateComponent<BarComponentB<'a>, Delegate = Provider<'a, FooParamA, FooParamB>>
+                + DelegateComponent<
+                    BarComponentC<FooParamB>,
+                    Delegate = Provider<'a, FooParamA, FooParamB>,
+                > + DelegateComponent<
+                    BarComponentD<BarParamA, FooParamA>,
+                    Delegate = Provider<'a, FooParamA, FooParamB>,
+                > + DelegateComponent<
+                    BarComponentE<'b, BarParamB, FooParamB>,
+                    Delegate = Provider<'a, FooParamA, FooParamB>,
+                >
+            {
+            }
+
+            impl<'a, FooParamA, FooParamB: FooConstraint, Components>
+                DelegatesToPreset<'a, FooParamA, FooParamB> for Components
+            where
+                Components: DelegateComponent<BarComponentA, Delegate = Provider<'a, FooParamA, FooParamB>>
+                    + DelegateComponent<BarComponentB<'a>, Delegate = Provider<'a, FooParamA, FooParamB>>
+                    + DelegateComponent<
+                        BarComponentC<FooParamB>,
+                        Delegate = Provider<'a, FooParamA, FooParamB>,
+                    > + DelegateComponent<
+                        BarComponentD<BarParamA, FooParamA>,
+                        Delegate = Provider<'a, FooParamA, FooParamB>,
+                    > + DelegateComponent<
+                        BarComponentE<'b, BarParamB, FooParamB>,
+                        Delegate = Provider<'a, FooParamA, FooParamB>,
+                    >,
+            {
+            }
+
+            #[macro_export]
+            macro_rules! with_foo_preset {
+                ($($body:tt)*) => {
+                    replace_with! { [BarComponentA, BarComponentB < 'a >, BarComponentC <
+                    FooParamB >, < BarParamA > BarComponentD < BarParamA, FooParamA >, < 'b,
+                    BarParamB : BarConstraint > BarComponentE < 'b, BarParamB, FooParamB >],
+                    $($body)* }
+                };
+            }
+
+            pub use with_foo_preset as with_components;
+        }
     };
 
     assert_equal_token_stream(&derived, &expected);
