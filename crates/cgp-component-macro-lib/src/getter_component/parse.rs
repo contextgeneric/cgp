977cb81dbc7ccdfd27876711411bb27dcfc18399
Soares Chen <soares.chen@maybevoid.com>
2025-03-23 13:22:36 +0000

    Break down parse_getter_fields into smaller functions

--- crates/cgp-component-macro-lib/src/getter_component/parse.rs
+++ crates/cgp-component-macro-lib/src/getter_component/parse.rs
@@ -1,10 +1,12 @@
 use alloc::vec::Vec;
 
 use quote::{quote, ToTokens};
+use syn::punctuated::Punctuated;
 use syn::spanned::Spanned;
+use syn::token::{Comma, Mut};
 use syn::{
     parse2, parse_quote, Error, FnArg, GenericArgument, Ident, ItemTrait, PathArguments,
-    ReturnType, TraitItem, Type, TypePath,
+    ReturnType, Signature, TraitItem, TraitItemFn, Type, TypePath,
 };
 
 use crate::derive_component::replace_self_type;
@@ -20,170 +22,9 @@ pub fn parse_getter_fields(
     for item in consumer_trait.items.iter() {
         match item {
             TraitItem::Fn(method) => {
-                let signature = &method.sig;
+                let getter_spec = parse_getter_method(context_type, method)?;
 
-                if signature.constness.is_some() {
-                    return Err(Error::new(
-                        signature.constness.span(),
-                        "getter method must not be const fn",
-                    ));
-                }
-
-                if signature.asyncness.is_some() {
-                    return Err(Error::new(
-                        signature.asyncness.span(),
-                        "getter method must not be async fn",
-                    ));
-                }
-
-                if signature.unsafety.is_some() {
-                    return Err(Error::new(
-                        signature.unsafety.span(),
-                        "getter method must not be unsafe fn",
-                    ));
-                }
-
-                if !signature.generics.params.is_empty() {
-                    return Err(Error::new(
-                        signature.generics.params.span(),
-                        "getter method must not contain generic param",
-                    ));
-                }
-
-                if signature.generics.where_clause.is_some() {
-                    return Err(Error::new(
-                        signature.generics.where_clause.span(),
-                        "getter method must not contain where clause",
-                    ));
-                }
-
-                let field_name = signature.ident.clone();
-
-                let args_count = signature.inputs.len();
-
-                let (arg, phantom) = if args_count == 1 {
-                    let [arg]: [&FnArg; 1] = signature
-                        .inputs
-                        .iter()
-                        .collect::<Vec<&FnArg>>()
-                        .try_into()
-                        .map_err(|_| {
-                            Error::new(
-                                signature.inputs.span(),
-                                "getter method must contain exactly one `&self` argument",
-                            )
-                        })?;
-
-                    (arg, None)
-                } else if args_count == 2 {
-                    let [arg, phantom_arg]: [&FnArg; 2] = signature
-                        .inputs
-                        .iter()
-                        .collect::<Vec<&FnArg>>()
-                        .try_into()
-                        .map_err(|_| {
-                            Error::new(
-                                signature.inputs.span(),
-                                "getter method must contain exactly one `&self` argument",
-                            )
-                        })?;
-
-                    match phantom_arg {
-                        FnArg::Typed(phantom_type) => (arg, Some(phantom_type.ty.as_ref().clone())),
-                        _ => {
-                            return Err(Error::new(
-                                signature.inputs.span(),
-                                "optional second argument in getter must be PhantomData",
-                            ));
-                        }
-                    }
-                } else {
-                    return Err(Error::new(
-                        signature.inputs.span(),
-                        "getter method must contain exactly one `&self` argument",
-                    ));
-                };
-
-                let field_mut = match arg {
-                    FnArg::Receiver(receiver) => {
-                        if receiver.reference.is_none() {
-                            return Err(Error::new(
-                                receiver.span(),
-                                "first argument to getter method must be a reference to self, i.e. `&self`"
-                            ));
-                        }
-
-                        receiver.mutability
-                    }
-                    _ => {
-                        return Err(Error::new(
-                            arg.span(),
-                            "first argument to getter method must be `&self`",
-                        ))
-                    }
-                };
-
-                let return_type = match &signature.output {
-                    ReturnType::Type(_, ty) => parse2(replace_self_type(
-                        ty.to_token_stream(),
-                        context_type,
-                        &Vec::new(),
-                    ))?,
-                    _ => {
-                        return Err(Error::new(
-                            signature.span(),
-                            "return type must be specified",
-                        ))
-                    }
-                };
-
-                let (field_type, field_mode) = match &return_type {
-                    Type::Reference(type_ref) => {
-                        if type_ref.mutability.is_some() != field_mut.is_some() {
-                            return Err(Error::new(
-                                type_ref.span(),
-                                "return type have the same mutability as the self reference",
-                            ));
-                        }
-
-                        if type_ref.elem.as_ref() == &parse_quote! { str } {
-                            // Special case to handle &str as String field
-
-                            let field_type: Type = parse_quote! { String };
-
-                            (field_type, FieldMode::Str)
-                        } else {
-                            let field_type: Type = type_ref.elem.as_ref().clone();
-
-                            (field_type, FieldMode::Reference)
-                        }
-                    }
-                    Type::Path(type_path) => {
-                        if let Some(field_type) = try_parse_option_ref(type_path) {
-                            (
-                                parse2(quote! { Option< #field_type > })?,
-                                FieldMode::OptionRef,
-                            )
-                        } else {
-                            (return_type.clone(), FieldMode::Clone)
-                        }
-                    }
-                    _ => {
-                        return Err(Error::new(
-                            return_type.span(),
-                            "return type must be a reference",
-                        ))
-                    }
-                };
-
-                fields.push(GetterField {
-                    field_name,
-                    field_type,
-                    return_type,
-                    field_mut,
-                    phantom_arg_type: phantom,
-                    field_mode,
-                })
+                fields.push(getter_spec);
             }
             _ => {
                 return Err(Error::new(
@@ -197,7 +38,187 @@ pub fn parse_getter_fields(
     Ok(fields)
 }
 
-pub fn try_parse_option_ref(type_path: &TypePath) -> Option<&Type> {
+fn parse_getter_method(context_type: &Ident, method: &TraitItemFn) -> syn::Result<GetterField> {
+    let signature = &method.sig;
+
+    validate_getter_method_signature(signature)?;
+
+    let field_name = signature.ident.clone();
+
+    let (arg, phantom) = parse_method_args(&signature.inputs)?;
+
+    let field_mut = parse_field_mut(arg)?;
+
+    let return_type = parse_return_type(context_type, &signature.output)?;
+
+    let (field_type, field_mode) = parse_field_type(&return_type, &field_mut)?;
+
+    Ok(GetterField {
+        field_name,
+        field_type,
+        return_type,
+        field_mut,
+        phantom_arg_type: phantom,
+        field_mode,
+    })
+}
+
+pub fn validate_getter_method_signature(signature: &Signature) -> syn::Result<()> {
+    if signature.constness.is_some() {
+        return Err(Error::new(
+            signature.constness.span(),
+            "getter method must not be const fn",
+        ));
+    }
+
+    if signature.asyncness.is_some() {
+        return Err(Error::new(
+            signature.asyncness.span(),
+            "getter method must not be async fn",
+        ));
+    }
+
+    if signature.unsafety.is_some() {
+        return Err(Error::new(
+            signature.unsafety.span(),
+            "getter method must not be unsafe fn",
+        ));
+    }
+
+    if !signature.generics.params.is_empty() {
+        return Err(Error::new(
+            signature.generics.params.span(),
+            "getter method must not contain generic param",
+        ));
+    }
+
+    if signature.generics.where_clause.is_some() {
+        return Err(Error::new(
+            signature.generics.where_clause.span(),
+            "getter method must not contain where clause",
+        ));
+    }
+
+    Ok(())
+}
+
+fn parse_method_args(args: &Punctuated<FnArg, Comma>) -> syn::Result<(&FnArg, Option<Type>)> {
+    let args_count = args.len();
+
+    if args_count == 1 {
+        let [arg] = parse_fixed_size_args::<1>(args)?;
+
+        Ok((arg, None))
+    } else if args_count == 2 {
+        let [arg, phantom_arg] = parse_fixed_size_args::<2>(args)?;
+
+        let phantom_arg_type = parse_phantom_arg_type(phantom_arg)?;
+
+        Ok((arg, Some(phantom_arg_type)))
+    } else {
+        return Err(Error::new(
+            args.span(),
+            "getter method must contain exactly one `&self` argument",
+        ));
+    }
+}
+
+fn parse_fixed_size_args<const I: usize>(
+    args: &Punctuated<FnArg, Comma>,
+) -> syn::Result<[&FnArg; I]> {
+    args.iter()
+        .collect::<Vec<&FnArg>>()
+        .try_into()
+        .map_err(|_| Error::new(args.span(), "expect getter method to contain {I} arguments"))
+}
+
+fn parse_phantom_arg_type(phantom_arg: &FnArg) -> syn::Result<Type> {
+    match phantom_arg {
+        FnArg::Typed(phantom_type) => Ok(phantom_type.ty.as_ref().clone()),
+        _ => {
+            return Err(Error::new(
+                phantom_arg.span(),
+                "optional second argument in getter must be PhantomData",
+            ));
+        }
+    }
+}
+
+fn parse_field_mut(arg: &FnArg) -> syn::Result<Option<Mut>> {
+    match arg {
+        FnArg::Receiver(receiver) => {
+            if receiver.reference.is_none() {
+                return Err(Error::new(
+                    receiver.span(),
+                    "first argument to getter method must be a reference to self, i.e. `&self`",
+                ));
+            }
+
+            Ok(receiver.mutability)
+        }
+        _ => {
+            return Err(Error::new(
+                arg.span(),
+                "first argument to getter method must be `&self`",
+            ))
+        }
+    }
+}
+
+fn parse_return_type(context_type: &Ident, return_type: &ReturnType) -> syn::Result<Type> {
+    match return_type {
+        ReturnType::Type(_, ty) => parse2(replace_self_type(
+            ty.to_token_stream(),
+            context_type,
+            &Vec::new(),
+        )),
+        _ => Err(Error::new(
+            return_type.span(),
+            "return type must be specified",
+        )),
+    }
+}
+
+fn parse_field_type(return_type: &Type, field_mut: &Option<Mut>) -> syn::Result<(Type, FieldMode)> {
+    match &return_type {
+        Type::Reference(type_ref) => {
+            if type_ref.mutability.is_some() != field_mut.is_some() {
+                return Err(Error::new(
+                    type_ref.span(),
+                    "return type have the same mutability as the self reference",
+                ));
+            }
+
+            if type_ref.elem.as_ref() == &parse_quote! { str } {
+                // Special case to handle &str as String field
+
+                let field_type: Type = parse_quote! { String };
+
+                Ok((field_type, FieldMode::Str))
+            } else {
+                let field_type: Type = type_ref.elem.as_ref().clone();
+
+                Ok((field_type, FieldMode::Reference))
+            }
+        }
+        Type::Path(type_path) => {
+            if let Some(field_type) = try_parse_option_ref(type_path) {
+                Ok((
+                    parse2(quote! { Option< #field_type > })?,
+                    FieldMode::OptionRef,
+                ))
+            } else {
+                Ok((return_type.clone(), FieldMode::Clone))
+            }
+        }
+        _ => Err(Error::new(
+            return_type.span(),
+            "return type must be a reference",
+        )),
+    }
+}
+
+fn try_parse_option_ref(type_path: &TypePath) -> Option<&Type> {
     let m_segment = type_path.path.segments.iter().next();
 
     if let Some(segment) = m_segment {
