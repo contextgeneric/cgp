9aebd1bd5a99e3e416ea9a757a70ed9ccecfced3
Soares Chen <soares.chen@maybevoid.com>
2025-03-29 19:45:34 +0000

    Merge cgp-field-macro into cgp-macro

--- crates/cgp-core/src/prelude.rs
+++ crates/cgp-core/src/prelude.rs
@@ -10,9 +10,9 @@ pub use cgp_error::{
 pub use cgp_field::{
     Char, Cons, Either, FieldGetter, HasField, HasFieldMut, MutFieldGetter, Nil, UseField, Void,
 };
-pub use cgp_field_macro::{product, symbol, HasField, Product, Sum};
 pub use cgp_macro::{
     cgp_auto_getter, cgp_component, cgp_context, cgp_getter, cgp_new_provider, cgp_preset,
-    cgp_provider, cgp_type, check_components, delegate_components, re_export_imports, replace_with,
+    cgp_provider, cgp_type, check_components, delegate_components, product, re_export_imports,
+    replace_with, symbol, HasField, Product, Sum,
 };
 pub use cgp_type::{HasType, ProvideType, UseType};
--- crates/cgp-macro/src/lib.rs
+++ crates/cgp-macro/src/lib.rs
@@ -98,3 +98,30 @@ pub fn replace_with(body: TokenStream) -> TokenStream {
         .unwrap_or_else(syn::Error::into_compile_error)
         .into()
 }
+
+#[proc_macro_derive(HasField)]
+pub fn derive_fields(item: TokenStream) -> TokenStream {
+    cgp_macro_lib::derive_fields(item.into()).into()
+}
+
+#[proc_macro]
+pub fn symbol(body: TokenStream) -> TokenStream {
+    cgp_macro_lib::make_symbol(body.into()).into()
+}
+
+#[proc_macro]
+#[allow(non_snake_case)]
+pub fn Product(body: TokenStream) -> TokenStream {
+    cgp_macro_lib::make_product_type(body.into()).into()
+}
+
+#[proc_macro]
+#[allow(non_snake_case)]
+pub fn Sum(body: TokenStream) -> TokenStream {
+    cgp_macro_lib::make_sum_type(body.into()).into()
+}
+
+#[proc_macro]
+pub fn product(body: TokenStream) -> TokenStream {
+    cgp_macro_lib::make_product_expr(body.into()).into()
+}
