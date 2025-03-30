ab48e872f225130bd5339d628fe2d0eb6b1503b7
Soares Chen <soares.chen@maybevoid.com>
2025-03-30 19:00:45 +0000

    Add more tests for enum

--- crates/cgp-tests/src/tests/has_fields/enum_fields.rs
+++ crates/cgp-tests/src/tests/has_fields/enum_fields.rs
@@ -16,18 +16,72 @@ fn test_simple_enum() {
 
         let person_a3 = Person::from_fields(person_a2);
         assert_eq!(person_a3, person_a1);
+
+        let person_a4 = person_a1.to_fields_ref();
+        assert_eq!(person_a4, Either::Left(Cons((&42).into(), Nil).into()));
     }
 
     {
-        let person_b1 = Person::Named("Alice".to_owned());
+        let name = "Alice".to_owned();
+
+        let person_b1 = Person::Named(name.clone());
 
         let person_b2 = person_b1.clone().to_fields();
         assert_eq!(
             person_b2,
-            Either::Right(Either::Left(Cons("Alice".to_owned().into(), Nil).into()))
+            Either::Right(Either::Left(Cons(name.clone().into(), Nil).into()))
         );
 
         let person_b3 = Person::from_fields(person_b2);
         assert_eq!(person_b3, person_b1);
+
+        let person_b4 = person_b1.to_fields_ref();
+        assert_eq!(
+            person_b4,
+            Either::Right(Either::Left(Cons((&name).into(), Nil).into()))
+        );
+    }
+}
+
+#[test]
+fn test_generic_enum() {
+    #[derive(Clone, Debug, Eq, PartialEq, HasFields)]
+    pub enum Person<'a, Name> {
+        Anonymous(u32),
+        Named(&'a Name),
+    }
+
+    {
+        let person_a1: Person<String> = Person::Anonymous(42);
+
+        let person_a2 = person_a1.clone().to_fields();
+        assert_eq!(person_a2, Either::Left(Cons(42.into(), Nil).into()));
+
+        let person_a3 = Person::from_fields(person_a2);
+        assert_eq!(person_a3, person_a1);
+
+        let person_a4 = person_a1.to_fields_ref();
+        assert_eq!(person_a4, Either::Left(Cons((&42).into(), Nil).into()));
+    }
+
+    {
+        let name = "Alice".to_owned();
+
+        let person_b1 = Person::Named(&name);
+
+        let person_b2 = person_b1.clone().to_fields();
+        assert_eq!(
+            person_b2,
+            Either::Right(Either::Left(Cons((&name).into(), Nil).into()))
+        );
+
+        let person_b3 = Person::from_fields(person_b2);
+        assert_eq!(person_b3, person_b1);
+
+        let person_b4 = person_b1.to_fields_ref();
+        assert_eq!(
+            person_b4,
+            Either::Right(Either::Left(Cons((&&name).into(), Nil).into()))
+        );
     }
 }
