1ff3c0fb6d0f297e079b20ae25b882c5be1c2850
Soares Chen <soares.chen@maybevoid.com>
2025-03-30 09:24:54 +0000

    Test use of to/from fields

--- crates/cgp-tests/src/tests/has_fields/named_struct.rs
+++ crates/cgp-tests/src/tests/has_fields/named_struct.rs
@@ -4,22 +4,29 @@ use cgp::prelude::*;
 
 #[test]
 fn test_single_named_field() {
-    #[derive(HasFields)]
+    #[derive(Clone, Debug, Eq, PartialEq, HasFields)]
     pub struct Person {
         pub name: String,
     }
 
     let name = "Alice".to_owned();
 
-    let person = Person { name: name.clone() };
+    let person1 = Person { name: name.clone() };
 
-    let product = person.to_fields();
+    let product = person1.clone().to_fields();
     assert_eq!(product, Cons(name.clone().into(), Nil));
+
+    let product_ref = person1.to_fields_ref();
+    assert_eq!(product_ref, Cons((&name).into(), Nil));
+
+    let person2 = Person::from_fields(product);
+
+    assert_eq!(person1, person2);
 }
 
 #[test]
 fn test_two_named_field() {
-    #[derive(HasFields)]
+    #[derive(Clone, Debug, Eq, PartialEq, HasFields)]
     pub struct Person {
         pub name: String,
         pub age: u8,
@@ -27,18 +34,25 @@ fn test_two_named_field() {
 
     let name = "Alice".to_owned();
 
-    let person = Person {
+    let person1 = Person {
         name: name.clone(),
         age: 32,
     };
 
-    let product = person.to_fields();
+    let product = person1.clone().to_fields();
     assert_eq!(product, Cons(name.clone().into(), Cons(32.into(), Nil)));
+
+    let product_ref = person1.to_fields_ref();
+    assert_eq!(product_ref, Cons((&name).into(), Cons((&32).into(), Nil)));
+
+    let person2 = Person::from_fields(product);
+
+    assert_eq!(person1, person2);
 }
 
 #[test]
 fn test_generic_struct() {
-    #[derive(HasFields)]
+    #[derive(Clone, Debug, Eq, PartialEq, HasFields)]
     pub struct Person<Name>
     where
         Name: Display,
@@ -49,11 +63,31 @@ fn test_generic_struct() {
 
     let name = "Alice".to_owned();
 
-    let person = Person {
+    let person1 = Person {
         name: name.clone(),
         age: 32,
     };
 
-    let product = person.to_fields();
+    let product = person1.clone().to_fields();
     assert_eq!(product, Cons(name.clone().into(), Cons(32.into(), Nil)));
+
+    let product_ref = person1.to_fields_ref();
+    assert_eq!(product_ref, Cons((&name).into(), Cons((&32).into(), Nil)));
+
+    let person2 = Person::from_fields(product);
+
+    assert_eq!(person1, person2);
 }
+
+// #[test]
+// fn test_single_unnamed_field() {
+//     #[derive(HasFields)]
+//     pub struct Person(String);
+
+//     let name = "Alice".to_owned();
+
+//     let person = Person { 0: name.clone() };
+
+//     let product = person.to_fields();
+//     assert_eq!(product, Cons(name.clone().into(), Nil));
+// }
