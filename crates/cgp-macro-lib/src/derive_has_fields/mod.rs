e250aeded1010c6c9f6d6b4ee5590a8f9436b73d
Soares Chen <soares.chen@maybevoid.com>
2025-03-30 08:59:54 +0000

    Remove extract_struct_fields

--- crates/cgp-macro-lib/src/derive_has_fields/mod.rs
+++ crates/cgp-macro-lib/src/derive_has_fields/mod.rs
@@ -1,9 +1,7 @@
-mod derive_enum;
+// mod derive_enum;
 mod derive_struct;
-mod field_tag;
 mod from_struct_fields;
 mod product;
-mod struct_fields;
 mod to_struct_fields;
 mod to_struct_fields_ref;
 
