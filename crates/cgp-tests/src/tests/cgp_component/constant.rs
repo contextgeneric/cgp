0a2554fa9cbaccf501c9768205171ccbda1bd68c
Soares Chen <soares.chen@maybevoid.com>
2025-05-05 19:24:02 +0000

    Add more tests for const

--- crates/cgp-tests/src/tests/cgp_component/constant.rs
+++ crates/cgp-tests/src/tests/cgp_component/constant.rs
@@ -7,4 +7,70 @@ pub fn test_component_with_const() {
     pub trait HasConstant {
         const CONSTANT: u64;
     }
+
+    pub struct UseConstant<const CONSTANT: u64>;
+
+    #[cgp_provider(ConstantGetterComponent)]
+    impl<Context, const CONSTANT: u64> ConstantGetter<Context> for UseConstant<CONSTANT> {
+        const CONSTANT: u64 = CONSTANT;
+    }
+
+    #[cgp_context(MyContextComponents)]
+    pub struct MyContext;
+
+    delegate_components! {
+        MyContextComponents {
+            ConstantGetterComponent: UseConstant<42>,
+        }
+    }
+
+    check_components! {
+        CanUseMyContext for MyContext {
+            ConstantGetterComponent,
+        }
+    }
+
+    assert_eq!(MyContext::CONSTANT, 42);
+}
+
+pub fn test_component_with_generic_const() {
+    #[cgp_type]
+    pub trait HasUnitType {
+        type Unit;
+    }
+
+    #[cgp_component {
+        provider: ConstantGetter
+    }]
+    pub trait HasConstant: HasUnitType {
+        const CONSTANT: Self::Unit;
+    }
+
+    pub struct UseConstant<const CONSTANT: u64>;
+
+    #[cgp_provider(ConstantGetterComponent)]
+    impl<Context, const CONSTANT: u64> ConstantGetter<Context> for UseConstant<CONSTANT>
+    where
+        Context: HasUnitType<Unit = u64>,
+    {
+        const CONSTANT: u64 = CONSTANT;
+    }
+
+    #[cgp_context(MyContextComponents)]
+    pub struct MyContext;
+
+    delegate_components! {
+        MyContextComponents {
+            UnitTypeProviderComponent: UseType<u64>,
+            ConstantGetterComponent: UseConstant<42>,
+        }
+    }
+
+    check_components! {
+        CanUseMyContext for MyContext {
+            ConstantGetterComponent,
+        }
+    }
+
+    assert_eq!(MyContext::CONSTANT, 42);
 }
