5887ecec3e2c5d28e8ebe7c466451e7a647b9a4e
Soares Chen <soares.chen@maybevoid.com>
2025-05-16 17:41:49 +0200

    Introduce cgp-handler crate

--- crates/cgp-extra/src/lib.rs
+++ crates/cgp-extra/src/lib.rs
@@ -1,3 +1,6 @@
 #![no_std]
 
-pub use {cgp_error_extra as error, cgp_inner as inner, cgp_run as run, cgp_runtime as runtime};
+pub use {
+    cgp_error_extra as error, cgp_handler as handler, cgp_inner as inner, cgp_run as run,
+    cgp_runtime as runtime,
+};
