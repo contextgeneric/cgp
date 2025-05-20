f4cbfc7d745bd6dd38d91571417e2c61d8c9e05e
Soares Chen <soares.chen@maybevoid.com>
2025-05-20 12:40:57 +0000

    Test use of Promote in test_pipe_handlers

--- crates/cgp-tests/src/tests/handler/pipe.rs
+++ crates/cgp-tests/src/tests/handler/pipe.rs
@@ -3,86 +3,10 @@ use core::marker::PhantomData;
 
 use cgp::core::error::ErrorTypeProviderComponent;
 use cgp::extra::handler::{
-    CanCompute, CanHandle, Computer, ComputerComponent, Handler, HandlerComponent, Pipe,
+    CanCompute, CanHandle, Computer, ComputerComponent, Handler, HandlerComponent, Pipe, Promote,
 };
 use cgp::prelude::*;
 use futures::executor::block_on;
-
-#[test]
-pub fn test_pipe_handlers() {
-    #[cgp_new_provider]
-    impl<Context, Tag, Field> Handler<Context, Tag, u64> for Multiply<Field>
-    where
-        Context: HasAsyncErrorType + HasField<Field, Value = u64>,
-        Tag: Send,
-    {
-        type Output = u64;
-
-        async fn handle(
-            context: &Context,
-            _tag: PhantomData<Tag>,
-            input: u64,
-        ) -> Result<Self::Output, Context::Error> {
-            Ok(input * context.get_field(PhantomData))
-        }
-    }
-
-    #[cgp_new_provider]
-    impl<Context, Tag, Field> Handler<Context, Tag, u64> for Add<Field>
-    where
-        Context: HasAsyncErrorType + HasField<Field, Value = u64>,
-        Tag: Send,
-    {
-        type Output = u64;
-
-        async fn handle(
-            context: &Context,
-            _tag: PhantomData<Tag>,
-            input: u64,
-        ) -> Result<Self::Output, Context::Error> {
-            Ok(input + context.get_field(PhantomData))
-        }
-    }
-
-    #[cgp_context]
-    #[derive(HasField)]
-    pub struct MyContext {
-        pub foo: u64,
-        pub bar: u64,
-        pub baz: u64,
-    }
-
-    delegate_components! {
-        MyContextComponents {
-            ErrorTypeProviderComponent: UseType<Infallible>,
-            HandlerComponent:
-                Pipe<
-                    Product! [
-                        Multiply<symbol!("foo")>,
-                        Add<symbol!("bar")>,
-                        Multiply<symbol!("baz")>,
-                    ]
-                >,
-        }
-    }
-
-    check_components! {
-        <Tag: Send>
-        CanUseMyContext for MyContext {
-            HandlerComponent: (Tag, u64),
-        }
-    }
-
-    let context = MyContext {
-        foo: 2,
-        bar: 3,
-        baz: 4,
-    };
-
-    let result = block_on(context.handle(PhantomData::<()>, 5)).unwrap();
-
-    assert_eq!(result, ((5 * 2) + 3) * 4);
-}
 
 #[test]
 pub fn test_pipe_computers() {
@@ -145,6 +69,77 @@ pub fn test_pipe_computers() {
     };
 
     let result = context.compute(PhantomData::<()>, 5);
+
+    assert_eq!(result, ((5 * 2) + 3) * 4);
+}
+
+#[test]
+pub fn test_pipe_handlers() {
+    #[cgp_new_provider]
+    impl<Context, Tag, Field> Handler<Context, Tag, u64> for Multiply<Field>
+    where
+        Context: HasAsyncErrorType + HasField<Field, Value = u64>,
+        Tag: Send,
+    {
+        type Output = u64;
+
+        async fn handle(
+            context: &Context,
+            _tag: PhantomData<Tag>,
+            input: u64,
+        ) -> Result<Self::Output, Context::Error> {
+            Ok(input * context.get_field(PhantomData))
+        }
+    }
+
+    #[cgp_new_provider]
+    impl<Context, Tag, Field> Computer<Context, Tag, u64> for Add<Field>
+    where
+        Context: HasField<Field, Value = u64>,
+    {
+        type Output = u64;
+
+        fn compute(context: &Context, _tag: PhantomData<Tag>, input: u64) -> u64 {
+            input + context.get_field(PhantomData)
+        }
+    }
+
+    #[cgp_context]
+    #[derive(HasField)]
+    pub struct MyContext {
+        pub foo: u64,
+        pub bar: u64,
+        pub baz: u64,
+    }
+
+    delegate_components! {
+        MyContextComponents {
+            ErrorTypeProviderComponent: UseType<Infallible>,
+            HandlerComponent:
+                Pipe<
+                    Product! [
+                        Multiply<symbol!("foo")>,
+                        Promote<Add<symbol!("bar")>>,
+                        Multiply<symbol!("baz")>,
+                    ]
+                >,
+        }
+    }
+
+    check_components! {
+        <Tag: Send>
+        CanUseMyContext for MyContext {
+            HandlerComponent: (Tag, u64),
+        }
+    }
+
+    let context = MyContext {
+        foo: 2,
+        bar: 3,
+        baz: 4,
+    };
+
+    let result = block_on(context.handle(PhantomData::<()>, 5)).unwrap();
 
     assert_eq!(result, ((5 * 2) + 3) * 4);
 }
