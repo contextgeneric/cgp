776475e7daa5b1baa5d7a26c1f40ba37f2f53675
Soares Chen <soares.chen@maybevoid.com>
2025-05-20 13:25:41 +0200

    Draft PipeHandlers

--- crates/cgp-handler/src/providers/pipe.rs
+++ crates/cgp-handler/src/providers/pipe.rs
@@ -4,6 +4,32 @@ use cgp_core::prelude::*;
 
 use crate::components::*;
 
+pub struct PipeHandlers<Handlers>(pub PhantomData<Handlers>);
+
+#[cgp_provider]
+impl<Context, Tag, CurrentHandler, RestHandlers> Handler<Context, Tag>
+    for PipeHandlers<Cons<CurrentHandler, RestHandlers>>
+where
+    Context: HasAsyncErrorType,
+    CurrentHandler: Handler<Context, Tag>,
+    PipeHandlers<RestHandlers>: Handler<Context, Tag>,
+    Tag: Send,
+    <PipeHandlers<RestHandlers> as Handler<Context, Tag>>::Input: From<CurrentHandler::Output>,
+{
+    type Input = CurrentHandler::Input;
+
+    type Output = <PipeHandlers<RestHandlers> as Handler<Context, Tag>>::Output;
+
+    async fn handle(
+        context: &Context,
+        tag: PhantomData<Tag>,
+        input: Self::Input,
+    ) -> Result<Self::Output, Context::Error> {
+        let intermediate = CurrentHandler::handle(context, tag, input).await?;
+        <PipeHandlers<RestHandlers>>::handle(context, tag, intermediate.into()).await
+    }
+}
+
 #[cgp_new_provider]
 impl<Context, Tag, HandlerA, HandlerB> Handler<Context, Tag> for PipeTwoHandlers<HandlerA, HandlerB>
 where
