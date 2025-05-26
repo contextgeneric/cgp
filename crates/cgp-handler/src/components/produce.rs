f1d783f252dfd7c5e1fbcf877b1945577d706480
Soares Chen <soares.chen@maybevoid.com>
2025-05-26 11:33:32 +0000

    Implement UseDelegate for Producer

--- crates/cgp-handler/src/components/produce.rs
+++ crates/cgp-handler/src/components/produce.rs
@@ -1,5 +1,6 @@
 use core::marker::PhantomData;
 
+use cgp_core::component::UseDelegate;
 use cgp_core::prelude::*;
 
 #[cgp_component(Producer)]
@@ -7,4 +8,19 @@ pub trait CanProduce<Tag> {
     type Output;
 
     fn produce(&self, _tag: PhantomData<Tag>) -> Self::Output;
+}
+
+#[cgp_provider]
+impl<Context, Tag, Components, Delegate> Producer<Context, Tag> for UseDelegate<Components>
+where
+    Context: HasAsyncErrorType,
+    Components: DelegateComponent<Tag, Delegate = Delegate>,
+    Delegate: Producer<Context, Tag>,
+    Tag: Send,
+{
+    type Output = Delegate::Output;
+
+    fn produce(context: &Context, tag: PhantomData<Tag>) -> Self::Output {
+        Delegate::produce(context, tag)
+    }
 }
