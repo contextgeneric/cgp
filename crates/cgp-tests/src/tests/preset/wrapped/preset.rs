48c09579929d690e8fc6d63fe4de08e9b388bdff
Soares Chen <soares.chen@maybevoid.com>
2025-05-27 20:22:14 +0000

    Test use of wrapped preset provider

--- crates/cgp-tests/src/tests/preset/wrapped/preset.rs
+++ crates/cgp-tests/src/tests/preset/wrapped/preset.rs
@@ -1,28 +1,21 @@
 #[cgp::re_export_imports]
 mod preset {
+    use core::convert::Infallible;
+    use core::error::Error;
+    use std::io::Error as IoError;
+
     use cgp::core::component::UseDelegate;
+    use cgp::extra::error::{RaiseFrom, RaiseInfallible, ReturnError};
     use cgp::prelude::*;
 
-    use crate::tests::preset::basic::components::{
-        BarTypeProviderComponent, FooTypeProviderComponent,
-    };
+    pub type BoxError = Box<dyn Error>;
 
     cgp_preset! {
         #[wrap_provider(UseDelegate)]
-        WrappedPreset {
-            [
-                FooTypeProviderComponent,
-                BarTypeProviderComponent,
-            ]:
-                UseType<()>,
+        ErrorHandlerPreset {
+            BoxError: ReturnError,
+            Infallible: RaiseInfallible,
+            IoError: RaiseFrom,
         }
     }
-
-    pub trait CheckDelegatesForMyPresetA:
-        DelegateComponent<FooTypeProviderComponent, Delegate = UseType<()>>
-        + DelegateComponent<BarTypeProviderComponent, Delegate = UseType<()>>
-    {
-    }
-
-    impl CheckDelegatesForMyPresetA for WrappedPreset::BaseProvider {}
 }
