a468b9cde448db18fdf5206a0e4fff42c1a0694f
Soares Chen <soares.chen@maybevoid.com>
2025-06-17 20:26:33 +0000

    Draft implement derive_build_field

--- crates/cgp-macro-lib/src/entrypoints/builder.rs
+++ crates/cgp-macro-lib/src/entrypoints/builder.rs
@@ -1,11 +1,14 @@
 use proc_macro2::{Span, TokenStream};
 use quote::{quote, ToTokens};
 use syn::punctuated::Punctuated;
+use syn::spanned::Spanned;
 use syn::token::{Colon, Comma};
 use syn::{
-    parse2, FieldValue, GenericParam, Generics, Ident, ItemImpl, ItemStruct, Member, Type,
-    TypeParam,
+    parse2, AngleBracketedGenericArguments, FieldValue, GenericArgument, GenericParam, Generics,
+    Ident, ItemImpl, ItemStruct, LitInt, Member, Type, TypeParam,
 };
+
+use crate::symbol::symbol_from_string;
 
 pub fn derive_builder(body: TokenStream) -> syn::Result<TokenStream> {
     let context_struct: ItemStruct = parse2(body)?;
@@ -99,4 +102,98 @@ pub fn derive_has_builder_impl(
     })?;
 
     Ok(item_impl)
+}
+
+pub fn derive_build_field_impls(
+    context_struct: &ItemStruct,
+    builder_struct: &ItemStruct,
+) -> syn::Result<Vec<ItemImpl>> {
+    let builder_ident = &builder_struct.ident;
+
+    let mut item_impls = Vec::new();
+
+    let base_generic_args: AngleBracketedGenericArguments =
+        parse2(context_struct.generics.split_for_impl().1.to_token_stream())?;
+
+    for (i, field) in context_struct.fields.iter().enumerate() {
+        let value_type = &field.ty;
+
+        let mut generics = context_struct.generics.clone();
+        let mut source_generic_args = base_generic_args.clone();
+        let mut output_generic_args = base_generic_args.clone();
+        let mut builder_fields = <Punctuated<FieldValue, Comma>>::new();
+
+        for (j, field) in context_struct.fields.iter().enumerate() {
+            let field_member = match &field.ident {
+                Some(ident) => Member::Named(ident.clone()),
+                None => Member::Unnamed(i.into()),
+            };
+
+            if j != i {
+                let generic_param_name = Ident::new(&format!("__F{}__", j), Span::call_site());
+                generics
+                    .params
+                    .push(parse2(quote! { #generic_param_name })?);
+
+                let generic_arg: GenericArgument = parse2(quote! { #generic_param_name })?;
+                source_generic_args.args.push(generic_arg.clone());
+                output_generic_args.args.push(generic_arg);
+
+                builder_fields.push(FieldValue {
+                    attrs: Vec::new(),
+                    member: field_member.clone(),
+                    colon_token: Some(Colon(Span::call_site())),
+                    expr: parse2(quote! { self. #field_member })?,
+                });
+            } else {
+                source_generic_args.args.push(parse2(quote! { IsNothing })?);
+                output_generic_args.args.push(parse2(quote! { IsPresent })?);
+
+                builder_fields.push(FieldValue {
+                    attrs: Vec::new(),
+                    member: field_member.clone(),
+                    colon_token: Some(Colon(Span::call_site())),
+                    expr: parse2(quote! { value })?,
+                });
+            }
+        }
+
+        let source_type: Type = parse2(quote! {
+            #builder_ident < #source_generic_args >
+        })?;
+
+        let output_type: Type = parse2(quote! {
+            <#builder_ident < #output_generic_args > as HasBuilder>::Builder
+        })?;
+
+        let tag_type = match &field.ident {
+            Some(ident) => symbol_from_string(&ident.to_string()),
+            None => {
+                let index = LitInt::new(&format!("{i}"), field.span());
+
+                parse2(quote! { Index< #index > })?
+            }
+        };
+
+        let (impl_generics, _, where_clause) = generics.split_for_impl();
+
+        let item_impl = parse2(quote! {
+            impl #impl_generics BuildField< #tag_type >
+                for #source_type
+            #where_clause
+            {
+                type Value = #value_type;
+
+                type Output = #output_type;
+
+                fn build_field(self, _tag: PhantomData< #tag_type >, value: Self::Value) -> Self::Output {
+                    todo!()
+                }
+            }
+        })?;
+
+        item_impls.push(item_impl);
+    }
+
+    Ok(item_impls)
 }
