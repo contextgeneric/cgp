fba4153516233eaf5ff8c292a9f4075dbc5fe56f
Soares Chen <soares.chen@maybevoid.com>
2025-06-18 11:09:05 +0000

    Implement derive HasExtractorRef

--- crates/cgp-macro-lib/src/derive_extractor/has_extractor_impl.rs
+++ crates/cgp-macro-lib/src/derive_extractor/has_extractor_impl.rs
@@ -15,7 +15,7 @@ pub fn derive_has_extractor_impl(
 
     let mut match_arms = Vec::<Arm>::new();
 
-    for (i, variant) in context_enum.variants.iter().enumerate() {
+    for variant in context_enum.variants.iter() {
         extractor_generics.args.push(parse2(quote! {
             IsPresent
         })?);
@@ -37,6 +37,58 @@ pub fn derive_has_extractor_impl(
             type Extractor = #extractor_ident #extractor_generics;
 
             fn extractor(self) -> Self::Extractor {
+                match self {
+                    #(#match_arms)*
+                }
+            }
+        }
+    })?;
+
+    Ok(item_impl)
+}
+
+pub fn derive_has_extractor_ref_impl(
+    context_enum: &ItemEnum,
+    extractor_ident: &Ident,
+) -> syn::Result<ItemImpl> {
+    let (impl_generics, ty_generics, where_clause) = context_enum.generics.split_for_impl();
+
+    let context_ident = &context_enum.ident;
+
+    let mut extractor_generics = to_generic_args(&context_enum.generics)?;
+    extractor_generics.args.insert(
+        0,
+        parse2(quote! {
+            'a
+        })?,
+    );
+
+    let mut match_arms = Vec::<Arm>::new();
+
+    for variant in context_enum.variants.iter() {
+        extractor_generics.args.push(parse2(quote! {
+            IsPresent
+        })?);
+
+        let variant_ident = &variant.ident;
+
+        match_arms.push(parse2(quote! {
+            Self :: #variant_ident ( value ) => {
+                #extractor_ident:: #variant_ident ( value )
+            }
+        })?);
+    }
+
+    let item_impl = parse2(quote! {
+        impl #impl_generics HasExtractorRef
+            for #context_ident #ty_generics
+        #where_clause
+        {
+            type ExtractorRef<'a> = #extractor_ident #extractor_generics
+            where
+                Self: 'a;
+
+            fn extractor_ref<'a>(&'a self) -> Self::ExtractorRef<'a> {
                 match self {
                     #(#match_arms)*
                 }
--- crates/cgp-macro-lib/src/entrypoints/derive_extract_field.rs
+++ crates/cgp-macro-lib/src/entrypoints/derive_extract_field.rs
@@ -2,7 +2,10 @@ use proc_macro2::TokenStream;
 use quote::quote;
 use syn::{parse2, Ident, ItemEnum};
 
-use crate::derive_extractor::{derive_extract_field_impls, derive_extractor_enum};
+use crate::derive_extractor::{
+    derive_extract_field_impls, derive_extractor_enum, derive_has_extractor_impl,
+    derive_has_extractor_ref_impl,
+};
 
 pub fn derive_extract_field(body: TokenStream) -> syn::Result<TokenStream> {
     let context_enum: ItemEnum = parse2(body)?;
@@ -18,6 +21,10 @@ pub fn derive_extract_field(body: TokenStream) -> syn::Result<TokenStream> {
     );
     let extractor_ref_enum = derive_extractor_enum(&context_enum, &extractor_ref_ident, true)?;
 
+    let has_extractor_impl = derive_has_extractor_impl(&context_enum, &extractor_ident)?;
+    let has_extractor_ref_impl =
+        derive_has_extractor_ref_impl(&context_enum, &extractor_ref_ident)?;
+
     let extractor_impls = derive_extract_field_impls(&context_enum, &extractor_ident, false)?;
     let extractor_ref_impls =
         derive_extract_field_impls(&context_enum, &extractor_ref_ident, true)?;
@@ -25,6 +32,9 @@ pub fn derive_extract_field(body: TokenStream) -> syn::Result<TokenStream> {
     let out = quote! {
         #extractor_enum
         #extractor_ref_enum
+
+        #has_extractor_impl
+        #has_extractor_ref_impl
 
         #(#extractor_impls)*
         #(#extractor_ref_impls)*
