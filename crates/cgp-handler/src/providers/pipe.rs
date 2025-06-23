b90c6b2cc70ac46b7d71da9d8380b8980181ab2f
Soares Chen <soares.chen@maybevoid.com>
2025-06-23 08:57:08 +0000

    Use helper traits to implement PipeHandlers

--- crates/cgp-handler/src/providers/pipe.rs
+++ crates/cgp-handler/src/providers/pipe.rs
@@ -7,12 +7,59 @@ use crate::components::*;
 pub struct PipeHandlers<Providers>(pub PhantomData<Providers>);
 
 #[cgp_provider]
-impl<Context, Code, Input, Output, CurrentProvider, RestProviders> Handler<Context, Code, Input>
-    for PipeHandlers<Cons<CurrentProvider, RestProviders>>
+impl<Context, Code, Input, Output, Providers> Handler<Context, Code, Input>
+    for PipeHandlers<Providers>
+where
+    Context: HasAsyncErrorType,
+    Providers: PipeHandler<Context, Code, Input, Output = Output>,
+    Code: Send,
+    Input: Send,
+    Output: Send,
+{
+    type Output = Output;
+
+    async fn handle(
+        context: &Context,
+        tag: PhantomData<Code>,
+        input: Input,
+    ) -> Result<Output, Context::Error> {
+        Providers::handle(context, tag, input).await
+    }
+}
+
+#[cgp_provider]
+impl<Context, Tag, Input, Output, Providers> Computer<Context, Tag, Input>
+    for PipeHandlers<Providers>
+where
+    Providers: PipeComputer<Context, Tag, Input, Output = Output>,
+{
+    type Output = Output;
+
+    fn compute(context: &Context, tag: PhantomData<Tag>, input: Input) -> Output {
+        Providers::compute(context, tag, input)
+    }
+}
+
+#[async_trait]
+trait PipeHandler<Context, Code, Input>
+where
+    Context: HasAsyncErrorType,
+{
+    type Output;
+
+    async fn handle(
+        context: &Context,
+        _code: PhantomData<Code>,
+        input: Input,
+    ) -> Result<Self::Output, Context::Error>;
+}
+
+impl<Context, Code, Input, Output, CurrentProvider, RestProviders> PipeHandler<Context, Code, Input>
+    for Cons<CurrentProvider, RestProviders>
 where
     Context: HasAsyncErrorType,
     CurrentProvider: Handler<Context, Code, Input>,
-    PipeHandlers<RestProviders>: Handler<Context, Code, CurrentProvider::Output, Output = Output>,
+    RestProviders: PipeHandler<Context, Code, CurrentProvider::Output, Output = Output>,
     Code: Send,
     Input: Send,
     Output: Send,
@@ -25,48 +72,51 @@ where
         input: Input,
     ) -> Result<Output, Context::Error> {
         let intermediate = CurrentProvider::handle(context, tag, input).await?;
-        <PipeHandlers<RestProviders>>::handle(context, tag, intermediate).await
+        RestProviders::handle(context, tag, intermediate).await
     }
 }
 
-#[cgp_provider]
-impl<Context, Tag, Input> Handler<Context, Tag, Input> for PipeHandlers<Nil>
+impl<Context, Code, Input> PipeHandler<Context, Code, Input> for Nil
 where
     Context: HasAsyncErrorType,
-    Tag: Send,
+    Code: Send,
     Input: Send,
 {
     type Output = Input;
 
     async fn handle(
         _context: &Context,
-        _tag: PhantomData<Tag>,
+        _code: PhantomData<Code>,
         input: Input,
     ) -> Result<Input, Context::Error> {
         Ok(input)
     }
 }
 
-#[cgp_provider]
-impl<Context, Tag, Input, Output, CurrentProvider, RestProviders> Computer<Context, Tag, Input>
-    for PipeHandlers<Cons<CurrentProvider, RestProviders>>
+trait PipeComputer<Context, Code, Input> {
+    type Output;
+
+    fn compute(context: &Context, _code: PhantomData<Code>, input: Input) -> Self::Output;
+}
+
+impl<Context, Tag, Input, Output, CurrentProvider, RestProviders> PipeComputer<Context, Tag, Input>
+    for Cons<CurrentProvider, RestProviders>
 where
     CurrentProvider: Computer<Context, Tag, Input>,
-    PipeHandlers<RestProviders>: Computer<Context, Tag, CurrentProvider::Output, Output = Output>,
+    RestProviders: PipeComputer<Context, Tag, CurrentProvider::Output, Output = Output>,
 {
     type Output = Output;
 
     fn compute(context: &Context, tag: PhantomData<Tag>, input: Input) -> Output {
         let intermediate = CurrentProvider::compute(context, tag, input);
-        <PipeHandlers<RestProviders>>::compute(context, tag, intermediate)
+        RestProviders::compute(context, tag, intermediate)
     }
 }
 
-#[cgp_provider]
-impl<Context, Tag, Input> Computer<Context, Tag, Input> for PipeHandlers<Nil> {
+impl<Context, Code, Input> PipeComputer<Context, Code, Input> for Nil {
     type Output = Input;
 
-    fn compute(_context: &Context, _tag: PhantomData<Tag>, input: Input) -> Input {
+    fn compute(_context: &Context, _code: PhantomData<Code>, input: Input) -> Input {
         input
     }
 }
