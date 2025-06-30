6729dc0bf7e8bc649a832a73384c0d51fcd3529f
Soares Chen <soares.chen@maybevoid.com>
2025-06-30 08:52:37 +0000

    Implement TryComputer for PipeHandlers

--- crates/cgp-handler/src/providers/pipe.rs
+++ crates/cgp-handler/src/providers/pipe.rs
@@ -28,6 +28,24 @@ where
 }
 
 #[cgp_provider]
+impl<Context, Tag, Input, Output, Providers> TryComputer<Context, Tag, Input>
+    for PipeHandlers<Providers>
+where
+    Context: HasErrorType,
+    Providers: PipeTryComputer<Context, Tag, Input, Output = Output>,
+{
+    type Output = Output;
+
+    fn try_compute(
+        context: &Context,
+        tag: PhantomData<Tag>,
+        input: Input,
+    ) -> Result<Output, Context::Error> {
+        Providers::try_compute(context, tag, input)
+    }
+}
+
+#[cgp_provider]
 impl<Context, Tag, Input, Output, Providers> Computer<Context, Tag, Input>
     for PipeHandlers<Providers>
 where
@@ -85,6 +103,53 @@ where
     type Output = Input;
 
     async fn handle(
+        _context: &Context,
+        _code: PhantomData<Code>,
+        input: Input,
+    ) -> Result<Input, Context::Error> {
+        Ok(input)
+    }
+}
+
+trait PipeTryComputer<Context, Code, Input>
+where
+    Context: HasErrorType,
+{
+    type Output;
+
+    fn try_compute(
+        context: &Context,
+        _code: PhantomData<Code>,
+        input: Input,
+    ) -> Result<Self::Output, Context::Error>;
+}
+
+impl<Context, Tag, Input, Output, CurrentProvider, RestProviders>
+    PipeTryComputer<Context, Tag, Input> for Cons<CurrentProvider, RestProviders>
+where
+    Context: HasErrorType,
+    CurrentProvider: TryComputer<Context, Tag, Input>,
+    RestProviders: PipeTryComputer<Context, Tag, CurrentProvider::Output, Output = Output>,
+{
+    type Output = Output;
+
+    fn try_compute(
+        context: &Context,
+        tag: PhantomData<Tag>,
+        input: Input,
+    ) -> Result<Output, Context::Error> {
+        let intermediate = CurrentProvider::try_compute(context, tag, input)?;
+        RestProviders::try_compute(context, tag, intermediate)
+    }
+}
+
+impl<Context, Code, Input> PipeTryComputer<Context, Code, Input> for Nil
+where
+    Context: HasErrorType,
+{
+    type Output = Input;
+
+    fn try_compute(
         _context: &Context,
         _code: PhantomData<Code>,
         input: Input,
