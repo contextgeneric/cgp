c9869ded47c923e5f0d3ceaf2e0b97415f768951
Soares Chen <soares.chen@maybevoid.com>
2025-07-05 17:29:30 +0000

    Merge ref version of MatchWithFieldHandlers

--- crates/cgp-dispatch/src/providers/matchers/match_with_field_handlers.rs
+++ crates/cgp-dispatch/src/providers/matchers/match_with_field_handlers.rs
@@ -1,13 +1,24 @@
 use cgp_core::prelude::*;
-use cgp_handler::{ComputerComponent, HandleFieldValue, HandlerComponent, TryComputerComponent};
+use cgp_handler::{
+    ComputerComponent, ComputerRefComponent, HandleFieldValue, HandlerComponent,
+    HandlerRefComponent, PromoteRef, TryComputerComponent, TryComputerRefComponent,
+};
 
-use crate::providers::matchers::to_field_handlers::ToInputFieldHandlers;
-use crate::MatchWithHandlers;
+use crate::providers::matchers::to_field_handlers::{
+    ToInputFieldHandlers, ToInputFieldHandlersRef,
+};
+use crate::{MatchWithHandlers, MatchWithHandlersRef};
 
 pub struct MatchWithFieldHandlers<Input, Provider = UseContext>(pub PhantomData<(Input, Provider)>);
 
 pub type MatchWithValueHandlers<Input, Provider = UseContext> =
     MatchWithFieldHandlers<Input, HandleFieldValue<Provider>>;
+
+pub type MatchWithFieldHandlersRef<Input, Provider = UseContext> =
+    MatchWithFieldHandlers<Input, PromoteRef<Provider>>;
+
+pub type MatchWithValueHandlersRef<Input, Provider = UseContext> =
+    MatchWithFieldHandlers<Input, HandleFieldValue<PromoteRef<Provider>>>;
 
 delegate_components! {
     <Input: ToInputFieldHandlers<Provider>, Provider>
@@ -18,5 +29,21 @@ delegate_components! {
             HandlerComponent,
         ]:
             MatchWithHandlers<Input::Handlers>,
+    }
+}
+
+delegate_components! {
+    <
+        Input: for<'a> ToInputFieldHandlersRef<'a, Provider, Handlers = Handlers>,
+        Provider,
+        Handlers,
+    >
+    MatchWithFieldHandlers<Input, Provider> {
+        [
+            ComputerRefComponent,
+            TryComputerRefComponent,
+            HandlerRefComponent,
+        ]:
+            PromoteRef<MatchWithHandlersRef<Handlers>>
     }
 }
--- crates/cgp-dispatch/src/providers/matchers/mod.rs
+++ crates/cgp-dispatch/src/providers/matchers/mod.rs
@@ -1,6 +1,4 @@
 mod match_with_field_handlers;
-mod match_with_field_handlers_ref;
 mod to_field_handlers;
 
 pub use match_with_field_handlers::*;
-pub use match_with_field_handlers_ref::*;
