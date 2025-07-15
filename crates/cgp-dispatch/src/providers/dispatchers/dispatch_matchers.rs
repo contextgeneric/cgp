027c587f756895c39f04a0e959ba4a35a77e364c
Soares Chen <soares.chen@maybevoid.com>
2025-07-15 13:34:13 +0000

    Define DispatchMatchers as a type alias

--- crates/cgp-dispatch/src/providers/dispatchers/dispatch_matchers.rs
+++ crates/cgp-dispatch/src/providers/dispatchers/dispatch_matchers.rs
@@ -1,19 +1,3 @@
-use core::marker::PhantomData;
-
-use cgp_core::prelude::*;
-use cgp_handler::{ComputerComponent, HandlerComponent, TryComputerComponent};
 use cgp_monad::{monads::ok::OkMonadic, providers::PipeMonadic};
 
-pub struct DispatchMatchers<Handlers>(pub PhantomData<Handlers>);
-
-delegate_components! {
-    <Providers>
-    DispatchMatchers<Providers> {
-        [
-            ComputerComponent,
-            TryComputerComponent,
-            HandlerComponent,
-        ]:
-            PipeMonadic<OkMonadic, Providers>,
-    }
-}
+pub type DispatchMatchers<Providers> = PipeMonadic<OkMonadic, Providers>;
