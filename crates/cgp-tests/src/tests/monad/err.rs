4e463b148ab273c1f6cfbcf52788473fec7ff402
Soares Chen <soares.chen@maybevoid.com>
2025-07-16 09:27:33 +0000

    Test use of BindErr

--- crates/cgp-tests/src/tests/monad/err.rs
+++ crates/cgp-tests/src/tests/monad/err.rs
@@ -1,4 +1,6 @@
-use cgp::extra::monad::monadic::err::{ComposeErr, ErrMonadic};
+use cgp::extra::handler::PipeHandlers;
+use cgp::extra::monad::monadic::err::{BindErr, ComposeErr, ErrMonadic};
+use cgp::extra::monad::monadic::ident::IdentMonadic;
 use cgp::extra::monad::providers::PipeMonadic;
 use cgp::prelude::*;
 
@@ -14,6 +16,20 @@ fn test_increment() {
 
     assert_eq!(Increment::compute(&context, code, 1), Ok(2));
     assert_eq!(Increment::compute(&context, code, 255), Err("overflow"));
+
+    assert_eq!(
+        PipeHandlers::<Product![Increment, BindErr<IdentMonadic, Increment>]>::compute(
+            &context, code, 1,
+        ),
+        Ok(3),
+    );
+
+    assert_eq!(
+        PipeHandlers::<Product![Increment, BindErr<IdentMonadic, Increment>]>::compute(
+            &context, code, 254,
+        ),
+        Err("overflow"),
+    );
 
     assert_eq!(
         ComposeErr::<Increment, Increment>::compute(&context, code, 1),
