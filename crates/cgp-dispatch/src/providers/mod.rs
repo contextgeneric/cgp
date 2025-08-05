bb59e82c64ac349b33f362da459dcde17b499f35
Soares Chen <soares.chen@maybevoid.com>
2025-08-05 14:40:02 +0000

    Reorganize WithHandlers constructs

--- crates/cgp-dispatch/src/providers/mod.rs
+++ crates/cgp-dispatch/src/providers/mod.rs
@@ -1,19 +1,13 @@
-mod build_with_handlers;
 mod builders;
 mod dispatchers;
 mod field_builders;
 mod field_matchers;
-mod match_first_with_handlers;
-mod match_with_handlers;
-mod match_with_handlers_ref;
 mod matchers;
+mod with_handlers;
 
-pub use build_with_handlers::*;
 pub use builders::*;
 pub use dispatchers::*;
 pub use field_builders::*;
 pub use field_matchers::*;
-pub use match_first_with_handlers::*;
-pub use match_with_handlers::*;
-pub use match_with_handlers_ref::*;
 pub use matchers::*;
+pub use with_handlers::*;
---
+++ crates/cgp-dispatch/src/providers/with_handlers/mod.rs
@@ -0,0 +1,9 @@
+mod build_with_handlers;
+mod match_first_with_handlers;
+mod match_with_handlers;
+mod match_with_handlers_ref;
+
+pub use build_with_handlers::*;
+pub use match_first_with_handlers::*;
+pub use match_with_handlers::*;
+pub use match_with_handlers_ref::*;
