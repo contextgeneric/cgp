d4f5e152df0be64cac7c3765adbcb0b360691b71
Soares Chen <soares.chen@maybevoid.com>
2025-08-05 14:22:50 +0000

    Return Args with Remainder in ExtractFirstFieldAndHandle

--- crates/cgp-dispatch/src/providers/field_matchers/extract_first_field.rs
+++ crates/cgp-dispatch/src/providers/field_matchers/extract_first_field.rs
@@ -12,16 +12,21 @@ where
     Input: ExtractField<Tag, Value = Value, Remainder = Remainder>,
     Provider: Computer<Context, Code, (Field<Tag, Value>, Args), Output = Output>,
 {
-    type Output = Result<Output, Remainder>;
+    type Output = Result<Output, (Remainder, Args)>;
 
     fn compute(
         context: &Context,
         tag: PhantomData<Code>,
         (input, args): (Input, Args),
-    ) -> Result<Output, Remainder> {
-        let value = input.extract_field(PhantomData::<Tag>)?;
-        let output = Provider::compute(context, tag, (value.into(), args));
-        Ok(output)
+    ) -> Result<Output, (Remainder, Args)> {
+        let res = input.extract_field(PhantomData::<Tag>);
+        match res {
+            Ok(value) => {
+                let output = Provider::compute(context, tag, (value.into(), args));
+                Ok(output)
+            }
+            Err(remainder) => Err((remainder, args)),
+        }
     }
 }
 
@@ -33,13 +38,13 @@ where
     Input: ExtractField<Tag, Value = Value, Remainder = Remainder>,
     Provider: TryComputer<Context, Code, (Field<Tag, Value>, Args), Output = Output>,
 {
-    type Output = Result<Output, Remainder>;
+    type Output = Result<Output, (Remainder, Args)>;
 
     fn try_compute(
         context: &Context,
         tag: PhantomData<Code>,
         (input, args): (Input, Args),
-    ) -> Result<Result<Output, Remainder>, Context::Error> {
+    ) -> Result<Result<Output, (Remainder, Args)>, Context::Error> {
         let value = input.extract_field(PhantomData::<Tag>);
 
         match value {
@@ -47,7 +52,7 @@ where
                 let output = Provider::try_compute(context, tag, (value.into(), args))?;
                 Ok(Ok(output))
             }
-            Err(remainder) => Ok(Err(remainder)),
+            Err(remainder) => Ok(Err((remainder, args))),
         }
     }
 }
@@ -69,13 +74,13 @@ where
     Input: ExtractField<Tag, Value = Value, Remainder = Remainder>,
     Provider: Handler<Context, Code, (Field<Tag, Value>, Args), Output = Output>,
 {
-    type Output = Result<Output, Remainder>;
+    type Output = Result<Output, (Remainder, Args)>;
 
     async fn handle(
         context: &Context,
         tag: PhantomData<Code>,
         (input, args): (Input, Args),
-    ) -> Result<Result<Output, Remainder>, Context::Error> {
+    ) -> Result<Result<Output, (Remainder, Args)>, Context::Error> {
         let value = input.extract_field(PhantomData::<Tag>);
 
         match value {
@@ -83,7 +88,7 @@ where
                 let output = Provider::handle(context, tag, (value.into(), args)).await?;
                 Ok(Ok(output))
             }
-            Err(remainder) => Ok(Err(remainder)),
+            Err(remainder) => Ok(Err((remainder, args))),
         }
     }
 }
