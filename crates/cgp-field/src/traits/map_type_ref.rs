141283e523b6de8ac386b6529f98a77d5a5c3889
Soares Chen <soares.chen@maybevoid.com>
2025-08-05 17:33:36 +0000

    Refactor MapTypeRef

---
+++ crates/cgp-field/src/traits/map_type_ref.rs
@@ -0,0 +1,21 @@
+pub trait MapTypeRef {
+    type Map<'a, T: 'a>: 'a;
+}
+
+pub struct IsRef;
+
+impl MapTypeRef for IsRef {
+    type Map<'a, T: 'a> = &'a T;
+}
+
+pub struct IsMut;
+
+impl MapTypeRef for IsMut {
+    type Map<'a, T: 'a> = &'a mut T;
+}
+
+pub struct IsOwned;
+
+impl MapTypeRef for IsOwned {
+    type Map<'a, T: 'a> = T;
+}
