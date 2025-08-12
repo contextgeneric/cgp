6d002f610f535d107820f9fe3339989c994b9e82
Soares Chen <soares.chen@maybevoid.com>
2025-08-12 15:04:36 +0000

    Add AsyncComputer traits

---
+++ crates/cgp-handler/src/components/async_computer.rs
@@ -0,0 +1,51 @@
+use core::marker::PhantomData;
+
+use cgp_core::component::UseDelegate;
+use cgp_core::prelude::*;
+
+use crate::UseInputDelegate;
+
+#[async_trait]
+#[cgp_component {
+    provider: AsyncComputer,
+    derive_delegate: [
+        UseDelegate<Code>,
+        UseInputDelegate<Input>,
+    ],
+}]
+pub trait CanComputeAsync<Code, Input> {
+    type Output;
+
+    async fn compute_async(&self, _code: PhantomData<Code>, input: Input) -> Self::Output;
+}
+
+#[async_trait]
+#[cgp_component {
+    provider: AsyncComputerRef,
+    derive_delegate: [
+        UseDelegate<Code>,
+        UseInputDelegate<Input>,
+    ],
+}]
+pub trait CanComputeAsyncRef<Code, Input> {
+    type Output;
+
+    async fn compute_async_ref(&self, _code: PhantomData<Code>, input: &Input) -> Self::Output;
+}
+
+#[cgp_provider]
+impl<Context: Async, Code: Send, Input: Send, Tag, Output> AsyncComputer<Context, Code, Input>
+    for UseField<Tag>
+where
+    Context: HasField<Tag>,
+    Context::Value: CanComputeAsync<Code, Input, Output = Output>,
+{
+    type Output = Output;
+
+    async fn compute_async(context: &Context, code: PhantomData<Code>, input: Input) -> Output {
+        context
+            .get_field(PhantomData)
+            .compute_async(code, input)
+            .await
+    }
+}
--- crates/cgp-handler/src/components/mod.rs
+++ crates/cgp-handler/src/components/mod.rs
@@ -1,9 +1,11 @@
-mod compute;
+mod async_computer;
+mod computer;
 mod handler;
 mod produce;
 mod try_compute;
 
-pub use compute::*;
+pub use async_computer::*;
+pub use computer::*;
 pub use handler::*;
 pub use produce::*;
 pub use try_compute::*;
