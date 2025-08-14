f0d83842eb64e0245e1f5bbaa4fcace82719ea50
Soares Chen <soares.chen@maybevoid.com>
2025-08-14 18:39:42 +0000

    Add test for generics

--- crates/cgp-tests/tests/dispatcher_macro_tests/generics.rs
+++ crates/cgp-tests/tests/dispatcher_macro_tests/generics.rs
@@ -1,28 +1,40 @@
+use core::fmt::Display;
+
 use cgp::prelude::*;
 
 use crate::dispatcher_macro_tests::types::{Bar, Foo, FooBar};
 
 #[cgp_dispatch]
-pub trait CanCall {
-    fn call<T>(&self, _a: u64, _b: bool) -> &'static str;
+pub trait CanCall<T> {
+    fn call_a(&self, _a: u64, _b: &T) -> String;
+
+    fn call_b(self, _a: u64, b: &mut T) -> &str;
 }
 
-impl CanCall for Foo {
-    fn call<T>(&self, _a: u64, _b: bool) -> &'static str {
+impl<T: Display> CanCall<T> for Foo {
+    fn call_a(&self, _a: u64, b: &T) -> String {
+        format!("foo-{}", b)
+    }
+
+    fn call_b(self, _a: u64, _b: &mut T) -> &str {
         "foo"
     }
 }
 
-impl CanCall for Bar {
-    fn call<T>(&self, _a: u64, _b: bool) -> &'static str {
+impl<T> CanCall<T> for Bar {
+    fn call_a(&self, _a: u64, _b: &T) -> String {
+        "bar".to_owned()
+    }
+
+    fn call_b(self, _a: u64, _b: &mut T) -> &str {
         "bar"
     }
 }
 
-pub trait CheckCanCallFooBar: CanCall {}
+pub trait CheckCanCallFooBar: CanCall<String> {}
 impl CheckCanCallFooBar for FooBar {}
 
 #[test]
 fn test_call_self_only() {
-    assert_eq!(FooBar::Foo(Foo).call(42, true), "foo");
+    assert_eq!(FooBar::Foo(Foo).call_a(42, &"extra"), "foo-extra");
 }
