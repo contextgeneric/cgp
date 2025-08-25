a154b33f238ca5d73b8d65b376ee90bd2a14f372
Soares Chen <soares.chen@maybevoid.com>
2025-08-25 19:47:24 +0000

    Add ?Sized bound to DelegateComponent's Name parameter

--- crates/cgp-component/src/traits/delegate_component.rs
+++ crates/cgp-component/src/traits/delegate_component.rs
@@ -43,6 +43,6 @@
     message = "{Self} does not contain any DelegateComponent entry for {Name}",
     note = "You might want to implement the provider trait for {Name} on {Self}"
 )]
-pub trait DelegateComponent<Name> {
+pub trait DelegateComponent<Name: ?Sized> {
     type Delegate;
 }
