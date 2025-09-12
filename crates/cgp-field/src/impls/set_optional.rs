ff4d2e7e95796bd389619690ea92b8957bf67afa
Soares Chen <soares.chen@maybevoid.com>
2025-09-12 15:13:24 +0000

    Implement SetOptional

---
+++ crates/cgp-field/src/impls/set_optional.rs
@@ -0,0 +1,35 @@
+use core::marker::PhantomData;
+
+use crate::impls::IsOptional;
+use crate::traits::UpdateField;
+
+pub trait SetOptional<Tag> {
+    type Value;
+
+    fn set(self, _tag: PhantomData<Tag>, value: Self::Value) -> Self;
+
+    fn set_optional(
+        self,
+        _tag: PhantomData<Tag>,
+        value: Self::Value,
+    ) -> (Option<Self::Value>, Self);
+}
+
+impl<Context, Tag> SetOptional<Tag> for Context
+where
+    Context: UpdateField<Tag, IsOptional, Mapper = IsOptional, Output = Context>,
+{
+    type Value = Context::Value;
+
+    fn set(self, tag: PhantomData<Tag>, value: Self::Value) -> Self {
+        self.set_optional(tag, value).1
+    }
+
+    fn set_optional(
+        self,
+        tag: PhantomData<Tag>,
+        value: Self::Value,
+    ) -> (Option<Self::Value>, Self) {
+        self.update_field(tag, Some(value))
+    }
+}
