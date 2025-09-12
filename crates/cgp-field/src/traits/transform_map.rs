45579d5133c1b716de7771a19f095fd850e7f6bf
Soares Chen <soares.chen@maybevoid.com>
2025-09-12 09:42:27 +0000

    Draft implement TransformMapFieldsImpl

--- crates/cgp-field/src/traits/transform_map.rs
+++ crates/cgp-field/src/traits/transform_map.rs
@@ -1,4 +1,11 @@
-use crate::MapType;
+use core::marker::PhantomData;
+
+use crate::{Cons, Field, IsNothing, MapType, Nil, UpdateField};
+
+/// Natural transformation from M1::Map<T> to M2::Map<T>
+pub trait TransformMap<M1: MapType, M2: MapType, T> {
+    fn transform_mapped(value: M1::Map<T>) -> M2::Map<T>;
+}
 
 pub trait TransformMapFields<Transform, TargetMap> {
     type Output;
@@ -6,7 +13,39 @@ pub trait TransformMapFields<Transform, TargetMap> {
     fn transform_map_fields(self) -> Self::Output;
 }
 
-/// Natural transformation from M1::Map<T> to M2::Map<T>
-pub trait TransformMap<M1: MapType, M2: MapType, T> {
-    fn transform_mapped(value: M1::Map<T>) -> M2::Map<T>;
+trait TransformMapFieldsImpl<Context, Transform, TargetMap> {
+    type Output;
+
+    fn transform_map_fields(context: Context) -> Self::Output;
+}
+
+impl<Context, Transform, TargetMap> TransformMapFieldsImpl<Context, Transform, TargetMap> for Nil {
+    type Output = Context;
+
+    fn transform_map_fields(context: Context) -> Self::Output {
+        context
+    }
+}
+
+impl<Tag, Value, Tail, ContextA, ContextB, ContextC, ContextD, Transform, TargetMap, SourceMap>
+    TransformMapFieldsImpl<ContextA, Transform, TargetMap> for Cons<Field<Tag, Value>, Tail>
+where
+    TargetMap: MapType,
+    SourceMap: MapType,
+    Tail: TransformMapFieldsImpl<ContextA, Transform, TargetMap, Output = ContextB>,
+    ContextB: UpdateField<Tag, IsNothing, Value = Value, Mapper = SourceMap, Output = ContextC>,
+    ContextC: UpdateField<Tag, TargetMap, Value = Value, Output = ContextD>,
+    Transform: TransformMap<SourceMap, TargetMap, Value>,
+{
+    type Output = ContextD;
+
+    fn transform_map_fields(context_a: ContextA) -> Self::Output {
+        let context_b = Tail::transform_map_fields(context_a);
+
+        let (value_a, context_c) = context_b.update_field(PhantomData, ());
+        let value_b = Transform::transform_mapped(value_a);
+        let (_, context_d) = context_c.update_field(PhantomData, value_b);
+
+        context_d
+    }
 }
