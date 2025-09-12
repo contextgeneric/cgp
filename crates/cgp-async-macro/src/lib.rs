44310a520aff9b12edbdf4a4fd83f342d59ca5f1
Soares Chen <soares.chen@maybevoid.com>
2025-09-12 13:02:21 +0000

    Remove #[strip_async] macro

--- crates/cgp-async-macro/src/lib.rs
+++ crates/cgp-async-macro/src/lib.rs
@@ -10,39 +10,15 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 
 mod impl_async;
-mod strip_async;
-
-/**
-   This macro can be used in place of the [`macro@native_async`] macro
-   to strip away all use of `async` and `.await` syntax. This helps emulate
-   async-generic by turnining async functions into sync functions.
-*/
-#[proc_macro_attribute]
-pub fn strip_async(_attr: TokenStream, stream: TokenStream) -> TokenStream {
-    strip_async::strip_async(stream.into()).into()
-}
 
 /**
     The `#[async_trait]` macro is used to desugar async functions in traits
-    to return `impl Future + Send`.
+    to return `impl Future`.
 
     This macro is required mainly to get around the current limitation of
     [async functions in traits](https://blog.rust-lang.org/2023/12/21/async-fn-rpit-in-traits/),
     which would produce a lint warning for `async_fn_in_trait` if bare async
     functions are defined in a trait.
-
-    The default approach adopted by CGP is to always include a `Send` bound
-    on the desugared `Future`, so that any async function that indirectly calls
-    the method can still implement `Send` be be used with libraries such as `tokio::spawn`.
-
-    It is possible to remove default `Send` by deactivating the `send` feature in the `cgp-async`
-    crate. However, the removal may be unreliable, as the feature flag could still be accidentally
-    enabled by one of the dependencies.
-
-    Note that although the macro shares the same name as the
-    [`async-trait`](https://docs.rs/async-trait) crate, it is implemented very differently.
-    The other crate with the same name returns a `Pin<Box<dyn Future + Send>>`, while
-    this macro returns a `impl Future + Send`.
 
     ## Example
 
@@ -59,7 +35,7 @@ pub fn strip_async(_attr: TokenStream, stream: TokenStream) -> TokenStream {
 
     ```rust,ignore
     pub trait CanRun {
-        fn run(&self) -> impl Future<Output = ()> + Send;
+        fn run(&self) -> impl Future<Output = ()>;
     }
     ```
 */
