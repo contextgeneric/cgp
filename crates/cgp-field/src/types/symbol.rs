fba6a8a592948d73fea3d3e2dd29412ce78e67d5
Soares Chen <soares.chen@maybevoid.com>
2025-09-16 19:29:33 +0000

    Implement traits for Symbol

--- crates/cgp-field/src/types/symbol.rs
+++ crates/cgp-field/src/types/symbol.rs
@@ -1,3 +1,32 @@
+use core::fmt::Display;
 use core::marker::PhantomData;
 
-pub struct Symbol<const LEN: usize, Bytes>(pub PhantomData<Bytes>);
+pub struct γ<const LEN: usize, Chars>(pub PhantomData<Chars>);
+
+pub use γ as Symbol;
+
+use crate::traits::StaticFormat;
+
+impl<const LEN: usize, Chars> Default for Symbol<LEN, Chars> {
+    fn default() -> Self {
+        Self(PhantomData)
+    }
+}
+
+impl<const LEN: usize, Chars> StaticFormat for Symbol<LEN, Chars>
+where
+    Chars: StaticFormat,
+{
+    fn fmt(f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        Chars::fmt(f)
+    }
+}
+
+impl<const LEN: usize, Chars> Display for Symbol<LEN, Chars>
+where
+    Self: StaticFormat,
+{
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        <Self as StaticFormat>::fmt(f)
+    }
+}
