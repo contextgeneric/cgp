d731609d361e5897f99f85973e228b9f23fb5214
Soares Chen <soares.chen@maybevoid.com>
2025-10-11 10:27:31 +0000

    Test non-self getter

--- crates/cgp-tests/src/tests/getter/non_self.rs
+++ crates/cgp-tests/src/tests/getter/non_self.rs
@@ -14,3 +14,30 @@ pub trait HasBarType {
 pub trait HasFooBar: HasFooType + HasBarType {
     fn foo_bar(foo: &Self::Foo) -> &Self::Bar;
 }
+
+#[cgp_context]
+pub struct App;
+
+#[derive(HasField)]
+pub struct Foo {
+    pub bar: u32,
+}
+
+delegate_components! {
+    AppComponents {
+        FooTypeProviderComponent:
+            UseType<Foo>,
+        BarTypeProviderComponent:
+            UseType<u32>,
+        FooBarGetterComponent:
+            UseField<Symbol!("bar")>,
+    }
+}
+
+#[test]
+fn test_non_self_getter() {
+    let foo = Foo { bar: 42 };
+
+    let bar = App::foo_bar(&foo);
+    assert_eq!(bar, &42);
+}
