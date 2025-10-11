da987ec1ff7cc95e621ec310b5ea6bdc40218db3
Soares Chen <soares.chen@maybevoid.com>
2025-10-11 10:56:11 +0000

    Remove `HasInner` trait

--- crates/cgp-extra/src/lib.rs
+++ crates/cgp-extra/src/lib.rs
@@ -4,6 +4,5 @@ pub mod prelude;
 
 pub use {
     cgp_dispatch as dispatch, cgp_error_extra as error, cgp_field_extra as field,
-    cgp_handler as handler, cgp_inner as inner, cgp_monad as monad, cgp_run as run,
-    cgp_runtime as runtime,
+    cgp_handler as handler, cgp_monad as monad, cgp_run as run, cgp_runtime as runtime,
 };
