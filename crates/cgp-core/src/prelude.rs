0818c416c4659db713aaa6f2134a6238595c255b
Soares Chen <soares.chen@maybevoid.com>
2025-10-11 12:16:25 +0000

    Remove lowercase `symbol!` macro

--- crates/cgp-core/src/prelude.rs
+++ crates/cgp-core/src/prelude.rs
@@ -18,8 +18,8 @@ pub use cgp_field::types::{
 };
 pub use cgp_macro::{
     BuildField, CgpData, CgpRecord, CgpVariant, ExtractField, FromVariant, HasField, HasFields,
-    Product, Sum, Symbol, Symbol as symbol, cgp_auto_getter, cgp_component, cgp_context,
-    cgp_getter, cgp_new_provider, cgp_preset, cgp_provider, cgp_type, check_components,
+    Product, Sum, Symbol, cgp_auto_getter, cgp_component, cgp_context, cgp_getter,
+    cgp_new_provider, cgp_preset, cgp_provider, cgp_type, check_components,
     delegate_and_check_components, delegate_components, product, re_export_imports, replace_with,
 };
 pub use cgp_type::{HasType, ProvideType, UseType};
