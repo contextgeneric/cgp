4c731aa19e2982b7cc26c418e13e0dc6e42dbba9
Soares Chen <soares.chen@maybevoid.com>
2025-10-17 19:46:14 +0000

    Move component tests to separate test unit

--- crates/cgp-tests/src/tests/mod.rs
+++ crates/cgp-tests/src/tests/mod.rs
@@ -1,11 +1,8 @@
 pub mod r#async;
 pub mod blanket_trait;
-pub mod cgp_component;
-pub mod cgp_impl;
 pub mod check_components;
 pub mod compose;
 pub mod delegate_and_check_components;
-pub mod delegate_components;
 pub mod getter;
 pub mod has_field;
 pub mod has_fields;
---
+++ crates/cgp-tests/tests/component_tests/mod.rs
@@ -0,0 +1,3 @@
+pub mod cgp_component;
+pub mod cgp_impl;
+pub mod delegate_components;
