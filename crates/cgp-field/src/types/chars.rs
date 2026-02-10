7d4203e110a661b272d9151726e9f2d4a4d7fe06
Soares Chen <soares.chen@maybevoid.com>
2026-02-10 17:45:38 +0100

    Try to hide Char alias from compiler messages

--- crates/cgp-field/src/types/chars.rs
+++ crates/cgp-field/src/types/chars.rs
@@ -50,6 +50,7 @@ use crate::traits::StaticFormat;
 #[derive(Eq, PartialEq, Clone, Copy, Default)]
 pub struct ζ<const CHAR: char, Tail>(pub PhantomData<Tail>);
 
+#[doc(hidden)]
 pub use ζ as Chars;
 
 impl<const CHAR: char, Tail> Display for Chars<CHAR, Tail>
