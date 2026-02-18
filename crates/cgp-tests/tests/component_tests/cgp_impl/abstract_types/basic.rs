01e812893b80049855d37dc234b2d62c4ddce391
Soares Chen <soares.chen@maybevoid.com>
2026-02-18 23:10:28 +0100

    Add basic test for `#[use_type]` in `#[cgp_impl]`

---
+++ crates/cgp-tests/tests/component_tests/cgp_impl/abstract_types/basic.rs
@@ -0,0 +1,41 @@
+use std::ops::Mul;
+
+use cgp::prelude::*;
+
+#[cgp_type]
+pub trait HasScalarType {
+    type Scalar;
+}
+
+#[cgp_component(AreaCalculator)]
+pub trait CanCalculateArea: HasScalarType {
+    fn area(&self) -> Self::Scalar;
+}
+
+#[cgp_impl(new RectangleArea)]
+#[use_type(HasScalarType::Scalar)]
+impl AreaCalculator
+where
+    Scalar: Mul<Output = Scalar> + Clone,
+{
+    fn area(&self, #[implicit] width: Scalar, #[implicit] height: Scalar) -> Scalar {
+        width * height
+    }
+}
+
+
+#[derive(HasField)]
+pub struct Rectangle {
+    pub width: f64,
+    pub height: f64,
+}
+
+delegate_and_check_components! {
+    CanUseRectangle for Rectangle;
+    Rectangle {
+        ScalarTypeProviderComponent:
+            UseType<f64>,
+        AreaCalculatorComponent:
+            RectangleArea,
+    }
+}
