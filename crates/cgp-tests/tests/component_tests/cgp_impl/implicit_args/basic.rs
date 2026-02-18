06bcfd9691f3ae6bcc7c5d5cf68838840761f117
Soares Chen <soares.chen@maybevoid.com>
2026-02-18 21:46:30 +0100

    Basic implicit test working

---
+++ crates/cgp-tests/tests/component_tests/cgp_impl/implicit_args/basic.rs
@@ -0,0 +1,27 @@
+use cgp::prelude::*;
+
+#[cgp_component(AreaCalculator)]
+pub trait CanCalculateArea {
+    fn area(&self) -> f64;
+}
+
+#[cgp_impl(new RectangleArea)]
+impl AreaCalculator {
+    fn area(&self, #[implicit] width: f64, #[implicit] height: f64) -> f64 {
+        width * height
+    }
+}
+
+#[derive(HasField)]
+pub struct Rectangle {
+    pub width: f64,
+    pub height: f64,
+}
+
+delegate_and_check_components! {
+    CanUseRectangle for Rectangle;
+    Rectangle {
+        AreaCalculatorComponent:
+            RectangleArea,
+    }
+}
--- crates/cgp-tests/tests/component_tests/cgp_impl/mod.rs
+++ crates/cgp-tests/tests/component_tests/cgp_impl/mod.rs
@@ -1,2 +1,3 @@
 pub mod basic;
+pub mod implicit_args;
 pub mod implicit_context;
