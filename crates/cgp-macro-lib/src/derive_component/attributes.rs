1799710fff0464db4febeb463b97d1728fba5231
Soares Chen <soares.chen@maybevoid.com>
2026-02-18 23:58:00 +0100

    Forbid abstract type equality in consumer trait

--- crates/cgp-macro-lib/src/derive_component/attributes.rs
+++ crates/cgp-macro-lib/src/derive_component/attributes.rs
@@ -20,9 +20,21 @@ pub fn parse_component_attributes(
                     .parse_args_with(Punctuated::<TypeParamBound, Comma>::parse_terminated)?;
                 parsed_attributes.extend.extend(extend_bound);
             } else if ident == "use_type" {
-                let use_type = attribute
+                let use_type_specs = attribute
                     .parse_args_with(Punctuated::<UseTypeSpec, Comma>::parse_terminated)?;
-                parsed_attributes.use_type.extend(use_type);
+
+                for use_type_spec in use_type_specs.iter() {
+                    for type_ident in use_type_spec.type_idents.iter() {
+                        if let Some(equals) = &type_ident.equals {
+                            return Err(syn::Error::new_spanned(
+                                &equals,
+                                "Type equality constraints cannot be used in component trait definition",
+                            ));
+                        }
+                    }
+                }
+
+                parsed_attributes.use_type.extend(use_type_specs);
             } else {
                 attributes.push(attribute);
             }
