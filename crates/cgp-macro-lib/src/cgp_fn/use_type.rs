6a182765c2b6b92426d3df6515be9a083e6d16c1
Soares Chen <soares.chen@maybevoid.com>
2026-02-18 12:30:58 +0100

    Draft implement abstract type equality bounds

--- crates/cgp-macro-lib/src/cgp_fn/use_type.rs
+++ crates/cgp-macro-lib/src/cgp_fn/use_type.rs
@@ -1,6 +1,9 @@
+use proc_macro2::TokenStream;
+use quote::{ToTokens, quote};
 use syn::parse::{Parse, ParseStream};
+use syn::punctuated::Punctuated;
 use syn::token::{As, Brace, Colon, Comma, Gt, Lt};
-use syn::{Ident, braced};
+use syn::{Ident, Type, TypeParamBound, braced, parse2};
 
 use crate::parse::SimpleType;
 
@@ -17,7 +20,7 @@ pub struct UseTypeIdent {
 impl UseTypeSpec {
     pub fn replace_ident(&self, ident: &Ident) -> Option<Ident> {
         for type_ident in &self.type_idents {
-            if type_ident.replacement_ident() == ident {
+            if type_ident.alias_ident() == ident {
                 let mut new_ident = type_ident.type_ident.clone();
                 new_ident.set_span(ident.span());
                 return Some(new_ident);
@@ -26,10 +29,94 @@ impl UseTypeSpec {
 
         None
     }
+
+    pub fn trait_bounds(
+        context_type: &TokenStream,
+        specs: &[Self],
+    ) -> syn::Result<Vec<TypeParamBound>> {
+        let mut bounds = Vec::new();
+
+        let mut past_specs = Vec::new();
+
+        for use_type in specs.iter() {
+            let type_equalities = use_type.find_type_equalities(context_type, &past_specs)?;
+
+            if type_equalities.is_empty() {
+                bounds.push(parse2(use_type.trait_path.to_token_stream())?);
+                past_specs.push(use_type);
+            } else {
+                let mut constraints: Punctuated<TokenStream, Comma> = Punctuated::new();
+
+                for (alias_ident, equal_target) in type_equalities.into_iter() {
+                    constraints.push(quote! {
+                        #alias_ident = #equal_target
+                    });
+                }
+
+                let trait_path = &use_type.trait_path;
+                let bound = quote! {
+                    #trait_path < #constraints >
+                };
+
+                bounds.push(parse2(bound)?);
+            }
+        }
+
+        Ok(bounds)
+    }
+
+    pub fn find_type_equalities(
+        &self,
+        context_type: &TokenStream,
+        past_specs: &[&UseTypeSpec],
+    ) -> syn::Result<Vec<(Ident, Type)>> {
+        let mut equalities = Vec::new();
+
+        for type_ident in self.type_idents.iter() {
+            if let Some(equality) = Self::find_type_equality(context_type, type_ident, past_specs)?
+            {
+                equalities.push(equality);
+            }
+        }
+
+        Ok(equalities)
+    }
+
+    pub fn find_type_equality(
+        context_type: &TokenStream,
+        current_ident: &UseTypeIdent,
+        past_specs: &[&UseTypeSpec],
+    ) -> syn::Result<Option<(Ident, Type)>> {
+        for past_spec in past_specs.iter() {
+            for type_ident in past_spec.type_idents.iter() {
+                let current_alias = current_ident.alias_ident();
+
+                if current_alias == type_ident.alias_ident() {
+                    if current_ident.as_alias.is_none() || type_ident.as_alias.is_none() {
+                        return Err(syn::Error::new_spanned(
+                            &current_ident.type_ident,
+                            "Multiple abstract types with the same name must be aliased explicitly with `as`",
+                        ));
+                    }
+
+                    let trait_path = &past_spec.trait_path;
+                    let type_ident = &current_ident.type_ident;
+
+                    let equal_target: Type = parse2(quote! {
+                        <#context_type as #trait_path>::#type_ident
+                    })?;
+
+                    return Ok(Some((current_alias.clone(), equal_target)));
+                }
+            }
+        }
+
+        Ok(None)
+    }
 }
 
 impl UseTypeIdent {
-    pub fn replacement_ident(&self) -> &Ident {
+    pub fn alias_ident(&self) -> &Ident {
         self.as_alias.as_ref().unwrap_or(&self.type_ident)
     }
 }
