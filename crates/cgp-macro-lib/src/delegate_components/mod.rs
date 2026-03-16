108255f0b3472af487577a222e42239b80df8059
Soares Chen <soares.chen@maybevoid.com>
2026-03-16 23:20:03 +0100

    Add derive_namespace_delegate helper

--- crates/cgp-macro-lib/src/delegate_components/mod.rs
+++ crates/cgp-macro-lib/src/delegate_components/mod.rs
@@ -1,8 +1,10 @@
 mod attributes;
 mod define_struct;
+mod derive_namespace;
 mod impl_delegate;
 mod merge_generics;
 
 pub use attributes::*;
 pub use define_struct::*;
+pub use derive_namespace::*;
 pub use impl_delegate::*;
--- crates/cgp-macro-lib/src/entrypoints/delegate_components.rs
+++ crates/cgp-macro-lib/src/entrypoints/delegate_components.rs
@@ -1,9 +1,10 @@
-use proc_macro2::{Span, TokenStream};
-use quote::{ToTokens, quote};
-use syn::{Ident, parse2};
+use proc_macro2::TokenStream;
+use quote::ToTokens;
+use syn::parse2;
 
 use crate::delegate_components::{
-    DelegateNamespaceAttribute, define_struct, impl_delegate_components, parse_delegate_attributes,
+    DelegateNamespaceAttribute, define_struct, derive_namespace_delegate, impl_delegate_components,
+    parse_delegate_attributes,
 };
 use crate::parse::{DelegateComponents, SimpleType, TypeGenerics};
 
@@ -28,45 +29,10 @@ pub fn delegate_components(body: TokenStream) -> syn::Result<TokenStream> {
     let attributes = parse_delegate_attributes(spec.attributes)?;
 
     if let Some(DelegateNamespaceAttribute { namespace }) = attributes.use_namespace {
-        let namespace =
-            namespace.unwrap_or_else(|| Ident::new("DefaultNamespace", Span::call_site()));
-
-        let mut generics = target_generics.generics.clone();
-        generics.params.push(parse2(quote! { __Component__ })?);
-
-        let impl_generics = generics.split_for_impl().0;
-
-        let namespace_impl = quote! {
-            impl #impl_generics
-                DelegateComponent<__Component__>
-                for #target_type
-            where
-                __Component__: #namespace< #target_type >,
-            {
-                type Delegate = < __Component__ as #namespace< #target_type >>::Provider;
-            }
-        };
+        let namespace_impl =
+            derive_namespace_delegate(namespace, target_type, &target_generics.generics)?;
 
         output.extend(namespace_impl);
-
-        let mut generics = generics.clone();
-        generics.params.push(parse2(quote! { __Context__ })?);
-        generics.params.push(parse2(quote! { __Params__ })?);
-
-        let impl_generics = generics.split_for_impl().0;
-
-        let is_provider_for_impl = quote! {
-            impl #impl_generics
-                IsProviderFor<__Component__, __Context__, __Params__>
-                for #target_type
-            where
-                __Component__: #namespace< #target_type >,
-                < __Component__ as #namespace< #target_type >>::Provider: IsProviderFor<__Component__, __Context__, __Params__>,
-            {
-            }
-        };
-
-        output.extend(is_provider_for_impl);
     }
 
     let impl_items = impl_delegate_components(&target_type, &target_generics, &spec.entries)?;
