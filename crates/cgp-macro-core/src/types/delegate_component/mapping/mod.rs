905dbe0cec6dccb8008a8b9bb260a3ceea79fd4a
Soares Chen <soares.chen@maybevoid.com>
2026-05-25 11:52:39 +0200

    Reorganize mapping constructs

--- crates/cgp-macro-core/src/types/delegate_component/mapping/mod.rs
+++ crates/cgp-macro-core/src/types/delegate_component/mapping/mod.rs
@@ -1,11 +1,11 @@
+mod combined;
 mod direct;
 mod eval;
-mod mapping;
 mod mode;
 mod normal;
 
+pub use combined::*;
 pub use direct::*;
 pub use eval::*;
-pub use mapping::*;
 pub use mode::*;
 pub use normal::*;
--- crates/cgp-macro-core/src/types/delegate_component/mod.rs
+++ crates/cgp-macro-core/src/types/delegate_component/mod.rs
@@ -1,13 +1,13 @@
 mod entries;
-mod entry;
 mod key;
+mod mapping;
 mod statement;
 mod table;
 mod value;
 
 pub use entries::*;
-pub use entry::*;
 pub use key::*;
+pub use mapping::*;
 pub use statement::*;
 pub use table::*;
 pub use value::*;
