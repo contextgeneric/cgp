3bf124d53d5859272c991755cccf3e6b84ad439c
Soares Chen <soares.chen@maybevoid.com>
2026-06-13 23:15:56 +0200

    Use ItemCgpComponent in cgp_getter

--- crates/macros/cgp-macro-lib/src/entrypoints/cgp_getter.rs
+++ crates/macros/cgp-macro-lib/src/entrypoints/cgp_getter.rs
@@ -1,11 +1,11 @@
-use cgp_macro_core::types::attributes::CgpComponentAttributes;
-use cgp_macro_core::types::cgp_component::CgpComponentRawArgs;
+use cgp_macro_core::types::cgp_component::{
+    CgpComponentRawArgs, EvaluatedCgpComponent, ItemCgpComponent,
+};
 use cgp_macro_core::types::provider_impl::derive_is_provider_for;
 use proc_macro2::TokenStream;
 use quote::quote;
 use syn::{Ident, ItemTrait, Type, parse_quote, parse2};
 
-use crate::derive_component::{derive_component_with_ast, preprocess_consumer_trait};
 use crate::derive_getter::{
     GetterField, derive_use_field_impl, derive_use_fields_impl, derive_with_provider_impl,
     parse_getter_fields,
@@ -14,34 +14,38 @@ use crate::derive_getter::{
 pub fn cgp_getter(attr: TokenStream, body: TokenStream) -> syn::Result<TokenStream> {
     let mut raw_args: CgpComponentRawArgs = parse2(attr.clone())?;
 
-    let mut consumer_trait: ItemTrait = syn::parse2(body)?;
-
-    let attributes = CgpComponentAttributes::parse(&mut consumer_trait.attrs)?;
-
-    preprocess_consumer_trait(&mut consumer_trait, &attributes)?;
+    let item_trait: ItemTrait = syn::parse2(body)?;
 
     if raw_args.provider_ident.is_none()
-        && let Some(field_name) = consumer_trait.ident.to_string().strip_prefix("Has")
+        && let Some(field_name) = item_trait.ident.to_string().strip_prefix("Has")
         && !field_name.is_empty()
     {
         raw_args.provider_ident = Some(Ident::new(
             &format!("{field_name}Getter"),
-            consumer_trait.ident.span(),
+            item_trait.ident.span(),
         ));
     }
 
-    let args = raw_args.try_into()?;
+    let item_cgp_component = ItemCgpComponent {
+        args: raw_args.try_into()?,
+        item_trait,
+    };
 
-    let derived_component = derive_component_with_ast(&args, consumer_trait.clone())?;
+    let evalutated = item_cgp_component.preprocess()?.eval()?;
+
+    let items = evalutated.to_items()?;
+
+    let EvaluatedCgpComponent {
+        args,
+        consumer_trait,
+        provider_trait,
+        ..
+    } = evalutated;
 
     let (fields, field_assoc_type) = parse_getter_fields(&args.context_ident, &consumer_trait)?;
 
-    let use_fields_impl = derive_use_fields_impl(
-        &args,
-        &derived_component.provider_trait,
-        &fields,
-        &field_assoc_type,
-    )?;
+    let use_fields_impl =
+        derive_use_fields_impl(&args, &provider_trait, &fields, &field_assoc_type)?;
 
     let component_name_type: Type = {
         let component_name = &args.component_name;
@@ -54,7 +58,7 @@ pub fn cgp_getter(attr: TokenStream, body: TokenStream) -> syn::Result<TokenStre
     let m_field: Option<[GetterField; 1]> = fields.try_into().ok();
 
     let mut derived = quote! {
-        #derived_component
+        #( #items )*
 
         #use_fields_impl
 
@@ -62,22 +66,14 @@ pub fn cgp_getter(attr: TokenStream, body: TokenStream) -> syn::Result<TokenStre
     };
 
     if let Some([field]) = m_field {
-        let use_field_impl = derive_use_field_impl(
-            &args,
-            &derived_component.provider_trait,
-            &field,
-            &field_assoc_type,
-        )?;
+        let use_field_impl =
+            derive_use_field_impl(&args, &provider_trait, &field, &field_assoc_type)?;
 
         let is_provider_use_field_impl =
             derive_is_provider_for(&component_name_type, &use_field_impl)?;
 
-        let use_provider_impl = derive_with_provider_impl(
-            &args,
-            &derived_component.provider_trait,
-            &field,
-            &field_assoc_type,
-        )?;
+        let use_provider_impl =
+            derive_with_provider_impl(&args, &provider_trait, &field, &field_assoc_type)?;
 
         let is_provider_use_provider_impl =
             derive_is_provider_for(&component_name_type, &use_provider_impl)?;
