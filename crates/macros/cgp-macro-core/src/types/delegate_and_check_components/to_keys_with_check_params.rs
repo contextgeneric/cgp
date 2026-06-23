5fd5ea5548a4ed621688f736c46096d16d7f5c19
Soares Chen <soares.chen@maybevoid.com>
2026-06-23 21:35:42 +0200

    Implement ToKeysWithCheckParams for MultiDelegateKey

--- crates/macros/cgp-macro-core/src/types/delegate_and_check_components/to_keys_with_check_params.rs
+++ crates/macros/cgp-macro-core/src/types/delegate_and_check_components/to_keys_with_check_params.rs
@@ -1,5 +1,5 @@
 use crate::types::delegate_and_check_components::{CheckParamsAttribute, KeyWithCheckParams};
-use crate::types::delegate_component::SingleDelegateKey;
+use crate::types::delegate_component::{DelegateKey, MultiDelegateKey, SingleDelegateKey};
 
 pub trait ToKeysWithCheckParams {
     fn to_keys_with_check_params(&self) -> syn::Result<Vec<KeyWithCheckParams>>;
@@ -15,5 +15,36 @@ impl ToKeysWithCheckParams for SingleDelegateKey {
         };
 
         Ok(vec![key])
+    }
+}
+
+impl ToKeysWithCheckParams for MultiDelegateKey {
+    fn to_keys_with_check_params(&self) -> syn::Result<Vec<KeyWithCheckParams>> {
+        let check_params = CheckParamsAttribute::parse_attributes(&self.attributes)?;
+
+        let mut out = Vec::new();
+
+        for key in &self.keys {
+            let inner_res = key.to_keys_with_check_params()?;
+            for inner in inner_res {
+                let inner_params = check_params.merge(&inner.check_params)?;
+                out.push(KeyWithCheckParams {
+                    key_type: inner.key_type,
+                    check_params: inner_params,
+                })
+            }
+        }
+
+        Ok(out)
+    }
+}
+
+impl ToKeysWithCheckParams for DelegateKey {
+    fn to_keys_with_check_params(&self) -> syn::Result<Vec<KeyWithCheckParams>> {
+        match self {
+            DelegateKey::Single(key) => key.to_keys_with_check_params(),
+            DelegateKey::Multi(key) => key.to_keys_with_check_params(),
+            DelegateKey::Path(_key) => Ok(Vec::new()),
+        }
     }
 }
