65f99960eecc532febb526424e83dff8896e6395
Soares Chen <soares.chen@maybevoid.com>
2026-06-28 12:57:39 +0200

    Remove `Parse` impl for `DeriveDelegateAttributes`

--- crates/macros/cgp-macro-core/src/types/attributes/derive_delegate/attributes.rs
+++ crates/macros/cgp-macro-core/src/types/attributes/derive_delegate/attributes.rs
@@ -1,30 +1,6 @@
-use syn::bracketed;
-use syn::parse::{Parse, ParseStream};
-use syn::punctuated::Punctuated;
-use syn::token::{Bracket, Comma};
-
 use crate::types::attributes::DeriveDelegateAttribute;
 
 #[derive(Default, Clone)]
 pub struct DeriveDelegateAttributes {
     pub attributes: Vec<DeriveDelegateAttribute>,
-}
-
-impl Parse for DeriveDelegateAttributes {
-    fn parse(input: ParseStream) -> syn::Result<Self> {
-        if input.peek(Bracket) {
-            let body;
-            bracketed!(body in input);
-
-            let attributes = <Punctuated<DeriveDelegateAttribute, Comma>>::parse_terminated(&body)?;
-            Ok(Self {
-                attributes: Vec::from_iter(attributes),
-            })
-        } else {
-            let spec = input.parse()?;
-            Ok(Self {
-                attributes: vec![spec],
-            })
-        }
-    }
 }
