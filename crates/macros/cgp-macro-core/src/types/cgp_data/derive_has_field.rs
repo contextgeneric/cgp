c99377ba8d2339e084fd21dadda48f60ba21e784
Soares Chen <soares.chen@maybevoid.com>
2026-06-28 22:01:39 +0200

    Add back derive_has_field_impls_from_struct

---
+++ crates/macros/cgp-macro-core/src/types/cgp_data/derive_has_field.rs
@@ -0,0 +1,110 @@
+use syn::spanned::Spanned;
+use syn::{Fields, ItemImpl, ItemStruct, LitInt};
+
+use crate::parse_internal;
+use crate::types::field::{Index, Symbol};
+
+pub fn derive_has_field_impls_from_struct(item_struct: &ItemStruct) -> syn::Result<Vec<ItemImpl>> {
+    let struct_ident = &item_struct.ident;
+
+    let (impl_generics, ty_generics, where_clause) = item_struct.generics.split_for_impl();
+
+    let mut item_impls = Vec::new();
+
+    match &item_struct.fields {
+        Fields::Named(fields) => {
+            for field in fields.named.iter() {
+                let field_ident = field.ident.as_ref().unwrap();
+
+                let field_symbol = Symbol::from_ident(field_ident.clone());
+
+                let field_type = &field.ty;
+
+                let has_field_impl: ItemImpl = parse_internal! {
+                    impl #impl_generics HasField< #field_symbol >
+                        for #struct_ident #ty_generics
+                    #where_clause
+                    {
+                        type Value = #field_type;
+
+                        fn get_field(
+                            &self,
+                            key: ::core::marker::PhantomData< #field_symbol >,
+                        ) -> &Self::Value
+                        {
+                            &self. #field_ident
+                        }
+                    }
+                };
+
+                let has_field_mut_impl: ItemImpl = parse_internal! {
+                    impl #impl_generics HasFieldMut< #field_symbol >
+                        for #struct_ident #ty_generics
+                    #where_clause
+                    {
+                        fn get_field_mut(
+                            &mut self,
+                            key: ::core::marker::PhantomData< #field_symbol >,
+                        ) -> &mut Self::Value
+                        {
+                            &mut self. #field_ident
+                        }
+                    }
+                };
+
+                item_impls.push(has_field_impl);
+                item_impls.push(has_field_mut_impl);
+            }
+        }
+        Fields::Unnamed(fields) => {
+            for (i, field) in fields.unnamed.iter().enumerate() {
+                let field_tag = Index {
+                    index: i,
+                    span: field.span(),
+                };
+
+                let field_ident = LitInt::new(&format!("{i}"), field.span());
+
+                let field_type = &field.ty;
+
+                let has_field_impl: ItemImpl = parse_internal! {
+                    impl #impl_generics HasField< #field_tag >
+                        for #struct_ident #ty_generics
+                    #where_clause
+                    {
+                        type Value = #field_type;
+
+                        fn get_field(
+                            &self,
+                            key: ::core::marker::PhantomData< #field_tag >,
+                        ) -> &Self::Value
+                        {
+                            &self. #field_ident
+                        }
+                    }
+                };
+
+                let has_field_mut_impl: ItemImpl = parse_internal! {
+                    impl #impl_generics HasFieldMut< #field_tag >
+                        for #struct_ident #ty_generics
+                    #where_clause
+                    {
+                        fn get_field_mut(
+                            &mut self,
+                            key: ::core::marker::PhantomData< #field_tag >,
+                        ) -> &mut Self::Value
+                        {
+                            &mut self. #field_ident
+                        }
+                    }
+                };
+
+                item_impls.push(has_field_impl);
+                item_impls.push(has_field_mut_impl);
+            }
+        }
+        _ => {}
+    }
+
+    Ok(item_impls)
+}
--- crates/macros/cgp-macro-core/src/types/cgp_data/mod.rs
+++ crates/macros/cgp-macro-core/src/types/cgp_data/mod.rs
@@ -1,9 +1,11 @@
 mod derive_builder;
 mod derive_extractor;
+mod derive_has_field;
 mod derive_has_fields;
 mod record;
 
 pub use derive_builder::*;
 pub use derive_extractor::*;
+pub use derive_has_field::*;
 pub use derive_has_fields::*;
 pub use record::*;
